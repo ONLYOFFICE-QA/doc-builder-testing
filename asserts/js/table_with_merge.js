9853e4123621d78a0724632cc73ee817be37e730
Ilya Karacharov <id.karacharov@mail.ru>
2016-05-12 14:05:03 +0300

    Add tests which checking tables rows and merge

---
+++ asserts/js/table_with_merge.js
@@ -0,0 +1,16 @@
+builder.CreateFile("docx");
+
+var Api = editor;
+var oDocument     = Api.GetDocument();
+oDocument.CreateNewHistoryPoint();
+
+var oParagraph, oTable, oTableRow, oCell, oCellContent, oRun, oDrawing;
+
+    oTable = Api.CreateTable(5, 5);
+    oDocument.Push(oTable);
+    oCell = oTable.MergeCells([oTable.GetRow(1).GetCell(1), oTable.GetRow(1).GetCell(2), oTable.GetRow(2).GetCell(1), oTable.GetRow(2).GetCell(2)]);
+    oCell.GetContent().GetElement(0).AddText("Merged cell");
+ 
+builder.SaveFile("docx", "/home/ilya/Documents/temp_docx/table_with_merge.docx");
+builder.CloseFile();
+
--- spec/add_new_row_spec.rb
+++ spec/add_new_row_spec.rb
@@ -5,13 +5,13 @@ require 'ooxml_parser'
 describe 'Add new row in table' do
 
   it 'Add new row in position' do
-    docx = OoxmlParser::DocxParser.parse_docx('/home/ilya/Documents/temp_docx/table_add_row_in_position.docx')
+    docx = OoxmlParser::DocxParser.parse_docx('/home/ilya/Documents/temp_docx/table_add_row_in_position.js.docx')
     expect(docx.elements[1].rows.length).to eq(4)
     expect(docx.elements[1].rows[1].cells.first.elements.first.character_style_array[1].text).to eq('NewRow')
   end
 
   it 'Add new row without position' do
-    docx = OoxmlParser::DocxParser.parse_docx('/home/ilya/Documents/temp_docx/table_add_row_without_position.docx')
+    docx = OoxmlParser::DocxParser.parse_docx('/home/ilya/Documents/temp_docx/table_add_row_without_position.js.docx')
     expect(docx.elements[1].rows.length).to eq(4)
     expect(docx.elements[1].rows[3].cells.first.elements.first.character_style_array[1].text).to eq('NewRow')
   end
