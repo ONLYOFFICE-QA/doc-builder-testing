f5143cec7c0e289b96b930c96a07d70601d52b5f
Ilya Karacharov <id.karacharov@mail.ru>
2016-05-13 11:00:13 +0300

    Add folders which contains specs and js

--- spec/Paragraph/Tables/table_with_merge_spec.rb
+++ spec/Paragraph/Tables/table_with_merge_spec.rb
@@ -5,7 +5,7 @@ require 'ooxml_parser'
 describe 'Add table with merge' do
 
   it 'Table with merge: 2:3 x 2x3' do
-    docx = OoxmlParser::DocxParser.parse_docx('/home/ilya/Documents/temp_docx/table_with_merge.js.docx')
+    docx = OoxmlParser::DocxParser.parse_docx('/home/ilya/Documents/temp_docx/table_with_merge.docx')
     expect(docx.elements[1].rows.length).to eq(5)
     expect(docx.elements[1].rows[1].cells[1].elements.first.character_style_array[1].text).to eq('Merged cell')
     expect(docx.elements[1].rows[1].cells[1].properties.merge.count_of_merged_cells).to eq(2)
