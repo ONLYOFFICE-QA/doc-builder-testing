a4417a38287664771173f5eb1917a88b211a3510
Anna Vybornova <gingetsu2007@yandex.ru>
2016-05-13 16:06:45 +0300

    1. add several js for section 2. Add and correct section test

--- spec/Sections/add_section_spec.rb
+++ spec/Sections/add_section_spec.rb
@@ -4,9 +4,17 @@ require 'ooxml_parser'
 
 describe 'Text with tabs' do
 
-  it 'Section' do
-    docx = OoxmlParser::DocxParser.parse_docx('/home/pc/Documents/temp_docx/section.docx')
-    expect(docx.elements[1].character_style_array.first.text).to eq('First Columns')
-    expect(docx.elements.last.character_style_array.first.text).to eq('Second Columns')
+  it 'Add Text in Section' do
+    docx = OoxmlParser::DocxParser.parse_docx('/home/pc/Documents/temp_docx/text_in_section.docx')
+    expect(docx.elements[1].character_style_array.first.text).to eq('First Page')
+    expect(docx.elements.last.character_style_array.first.text).to eq('Second Page')
+  end
+
+  it 'Add Table in Section' do
+    docx = OoxmlParser::DocxParser.parse_docx('/home/pc/Documents/temp_docx/table_in_section.docx')
+    expect(docx.element_by_description[1].rows.length).to eq(3)
+    expect(docx.element_by_description[1].rows[0].cells.length).to eq(3)
+    expect(docx.element_by_description.last.rows.length).to eq(3)
+    expect(docx.element_by_description.last.rows[0].cells.length).to eq(3)
   end
 end
