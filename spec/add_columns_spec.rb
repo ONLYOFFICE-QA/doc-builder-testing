d2491448899d3926b6db54b7275d54d5f6e989fc
Anna Vybornova <gingetsu2007@yandex.ru>
2016-05-13 11:08:12 +0300

    add section test and correct columns

--- spec/add_columns_spec.rb
+++ spec/add_columns_spec.rb
@@ -8,17 +8,23 @@ describe 'Text with tabs' do
     docx = OoxmlParser::DocxParser.parse_docx('/home/pc/Documents/temp_docx/one_column.docx')
     expect(docx.page_properties.columns.count).to eq(1)
     expect(docx.page_properties.columns.equal_width).to be_truthy
+    expect(docx.elements[1].character_style_array.first.text).to eq('First Columns')
   end
 
   it 'Two Columns' do
     docx = OoxmlParser::DocxParser.parse_docx('/home/pc/Documents/temp_docx/two_columns.docx')
     expect(docx.page_properties.columns.count).to eq(2)
     expect(docx.page_properties.columns.equal_width).to be_truthy
+    expect(docx.elements[1].character_style_array.first.text).to eq('First Columns')
+    expect(docx.elements[2].character_style_array.first.text).to eq('Second Columns')
   end
 
   it 'Three Columns' do
     docx = OoxmlParser::DocxParser.parse_docx('/home/pc/Documents/temp_docx/three_columns.docx')
     expect(docx.page_properties.columns.count).to eq(3)
     expect(docx.page_properties.columns.equal_width).to be_truthy
+    expect(docx.elements[1].character_style_array.first.text).to eq('First Columns')
+    expect(docx.elements[2].character_style_array.first.text).to eq('Second Columns')
+    expect(docx.elements[3].character_style_array.first.text).to eq('Third Columns')
   end
 end
