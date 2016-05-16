03410b160c4619e544da532cb023af61de1e8902
Pavel Lobashov <shockwavenn@gmail.com>
2016-05-16 13:22:53 +0300

    Add test for output path is incorrect

--- spec/doc_builder_wrapper_spec.rb
+++ spec/doc_builder_wrapper_spec.rb
@@ -7,4 +7,8 @@ describe 'My behaviour' do
   it 'should raise correct error if input file is incorrect' do
     expect(builder.build_doc('test')).to eq("error: cannot read run file\n")
   end
+
+  it 'should raise error if output path is incorrect' do
+    expect(builder.build_doc('asserts/js/Paragraph/Text/add_text_in_paragraph.js')).to eq("error: : save file error\n")
+  end
 end
