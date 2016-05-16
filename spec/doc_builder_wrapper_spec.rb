2f7ea9678a64cd2136d69c82ac7957fca8ee24cb
Pavel Lobashov <shockwavenn@gmail.com>
2016-05-16 13:36:05 +0300

    Introduce variable simple script

--- spec/doc_builder_wrapper_spec.rb
+++ spec/doc_builder_wrapper_spec.rb
@@ -3,12 +3,13 @@ require_relative '../lib/doc_builder_testing'
 
 describe 'My behaviour' do
   let(:builder) { DocBuilderWrapper.new }
+  let(:simple_script) { 'asserts/js/Paragraph/Text/add_text_in_paragraph.js' }
 
   it 'should raise correct error if input file is incorrect' do
     expect(builder.build_doc('test')).to eq("error: cannot read run file\n")
   end
 
   it 'should raise error if output path is incorrect' do
-    expect(builder.build_doc('asserts/js/Paragraph/Text/add_text_in_paragraph.js')).to eq("error: : save file error\n")
+    expect(builder.build_doc(simple_script)).to eq("error: : save file error\n")
   end
 end
