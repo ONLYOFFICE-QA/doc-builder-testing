64f2ed6984ad9a77f71b214a703a9de74eebed93
Pavel Lobashov <shockwavenn@gmail.com>
2016-05-16 13:38:34 +0300

    Add method to duplicate file with scrip
    It change output path to temp file

--- lib/doc_builder_testing/doc_builder_wrapper.rb
+++ lib/doc_builder_testing/doc_builder_wrapper.rb
@@ -1,4 +1,5 @@
 # Class for Wrapping doc building
+require 'tempfile'
 class DocBuilderWrapper
   # Path to Builder folder
   attr_accessor :builder_home
@@ -12,5 +13,15 @@ class DocBuilderWrapper
 
   def build_doc(script_file)
     `LD_LIBRARY_PATH=#{@builder_home} #{@builder_exe} #{script_file} 2>&1`
+  end
+
+  def self.change_output_file(script_file)
+    temp_output_file = Tempfile.new('doc_builder_wrapper_output')
+    script_file_content = File.open(script_file, "rb").read
+    script_file_content.gsub!(/^builder\.SaveFile.*$/, "builder.SaveFile(\"docx\", \"#{temp_output_file.path}\");")
+    temp_script_file = Tempfile.new('doc_builder_wrapper_script')
+    temp_script_file.write(script_file_content)
+    temp_script_file.close
+    {temp_script_file: temp_script_file, temp_output_file: temp_output_file}
   end
 end
--- spec/doc_builder_wrapper_spec.rb
+++ spec/doc_builder_wrapper_spec.rb
@@ -12,4 +12,18 @@ describe 'My behaviour' do
   it 'should raise error if output path is incorrect' do
     expect(builder.build_doc(simple_script)).to eq("error: : save file error\n")
   end
+
+  describe 'change_output_file' do
+    it 'check that change output file do not change original file' do
+      before_change = File.open(simple_script, 'rb').read
+      DocBuilderWrapper.change_output_file(simple_script)
+      after_change = File.open(simple_script, 'rb').read
+      expect(before_change).to eq(after_change)
+    end
+
+    it 'check that changed file contain returned values' do
+      rebuild_result = DocBuilderWrapper.change_output_file(simple_script)
+      expect(File.open(rebuild_result[:temp_script_file], 'rb').read).to include(rebuild_result[:temp_output_file].path)
+    end
+  end
 end
