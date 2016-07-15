065e4b89ecf6daf7a35d76e3af099dd96d8a4788
Dmitriy Rotatyy <kvazilife@gmail.com>
2016-07-15 17:06:48 +0300

    Add tests for all API section in document bilder doc. It need for check basic function of Api methods. Fix one js file because it was wrong

--- asserts/js/docx/smoke/Api/createpresetcolor.js
+++ asserts/js/docx/smoke/Api/createpresetcolor.js
@@ -1,12 +1,10 @@
 builder.CreateFile("docx");
 var oDocument = Api.GetDocument();
-var oGs1, oGs2, oFill, oStroke, oDrawing, oParagraph;
+var oFill, oStroke, oDrawing, oParagraph;
 oParagraph = oDocument.GetElement(0);
-oGs1 = Api.CreateGradientStop(Api.CreatePresetColor("lightYellow"), 0);
-oGs2 = Api.CreateGradientStop(Api.CreateRGBColor(255, 164, 101), 100000);
-oFill = Api.CreateRadialGradientFill([oGs1, oGs2]);
+oFill = Api.CreatePatternFill("dashDnDiag", Api.CreateRGBColor(255, 224, 204), Api.CreateRGBColor(255, 164, 101));
 oStroke = Api.CreateStroke(0, Api.CreateNoFill());
 oDrawing = Api.CreateShape("rect", 5930900, 395605, oFill, oStroke);
 oParagraph.AddDrawing(oDrawing);
-builder.SaveFile("docx", "CreatePresetColor.docx");
+builder.SaveFile("docx", "CreatePatternFill.docx");
 builder.CloseFile();
--- spec/docx/smoke/Api_spec.rb
+++ spec/docx/smoke/Api_spec.rb
@@ -55,12 +55,12 @@ describe 'Api section tests' do
 
   it 'Api | CreateNoFill method' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/Api/createnofill.js')
-    expect(docx.nil?).to eq(false)
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2007_content.data.properties.stroke_color.nil?).to be_truthy
   end
 
   it 'Api | CreateParagraph method' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/Api/createparagraph.js')
-    expect(docx.nil?).to eq(false)
+    expect(docx.elements[1].character_style_array.first.text).to eq('This is a new paragraph')
   end
 
   it 'Api | CreatePatternFill method' do
@@ -70,46 +70,70 @@ describe 'Api section tests' do
 
   it 'Api | CreatePresetColor method' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/Api/createpresetcolor.js')
-    expect(docx.nil?).to eq(false)
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.fill_color.value.preset).to eq(:dashDnDiag)
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.fill_color.value.background_color).to eq(OoxmlParser::Color.new(255, 224, 204))
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.fill_color.value.foreground_color).to eq(OoxmlParser::Color.new(255, 164, 101))
   end
 
   it 'Api | CreateRadialGradientFill method' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/Api/createradialgradientfill.js')
-    expect(docx.nil?).to eq(false)
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.fill_color.value.path).to eq(:circle)
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.fill_color.value.gradient_stops[0].color.to_s).to eq('RGB (255, 224, 204)')
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.fill_color.value.gradient_stops[0].position).to eq(0.0)
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.fill_color.value.gradient_stops[1].color.to_s).to eq('RGB (255, 164, 101)')
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.fill_color.value.gradient_stops[1].position).to eq(100.0)
   end
 
   it 'Api | CreateRGBColor method' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/Api/creatergbcolor.js')
-    expect(docx.nil?).to eq(false)
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.fill_color.value.gradient_stops[0].color.to_s).to eq('RGB (255, 224, 204)')
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.fill_color.value.gradient_stops[0].position).to eq(0.0)
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.fill_color.value.gradient_stops[1].color.to_s).to eq('RGB (255, 164, 101)')
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.fill_color.value.gradient_stops[1].position).to eq(100.0)
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.fill_color.value.nil?).to be_falsey
   end
 
   it 'Api | CreateRun method' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/Api/createrun.js')
-    expect(docx.nil?).to eq(false)
+    expect(docx.elements.first.character_style_array.size).to eq(3)
   end
 
   it 'Api | CreateSchemeColor method' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/Api/createschemecolor.js')
-    expect(docx.nil?).to eq(false)
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.fill_color.value.scheme).to eq('accent6')
   end
 
   it 'Api | CreateShape method' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/Api/createshape.js')
-    expect(docx.nil?).to eq(false)
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.preset_geometry.name).to eq(:rect)
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.shape_size.extent.x).to eq(5_930_900.0)
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.shape_size.extent.y).to eq(395_605.0)
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2007_content.data.properties.stroke_color.nil?).to be_truthy
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.fill_color.value.nil?).to be_falsey
   end
 
   it 'Api | CreateSolidFill method' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/Api/createsolidfill.js')
-    expect(docx.nil?).to eq(false)
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.fill_color.value).to eq(OoxmlParser::Color.new(0, 255, 0))
   end
 
   it 'Api | CreateStroke method' do
+    pending 'Documentation error'
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/Api/createstroke.js')
-    expect(docx.nil?).to eq(false)
+    expect(docx.elements.first.character_style_array[1].alternate_content.office2007_content.data.properties.stroke_color).to eq(OoxmlParser::Color.new(255, 224, 204))
   end
 
   it 'Api | CreateTable method' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/Api/createtable.js')
-    expect(docx.nil?).to eq(false)
+    expect(docx.elements[1].rows.size).to eq(3)
+    expect(docx.elements[1].rows[0].cells.size).to eq(3)
+    expect(docx.elements[1].rows[1].cells.size).to eq(3)
+    expect(docx.elements[1].rows[2].cells.size).to eq(3)
+    expect(docx.elements[1].properties.table_borders.bottom.color).to eq(OoxmlParser::Color.new(0,0,255))
+    expect(docx.elements[1].properties.table_borders.left.color).to eq(OoxmlParser::Color.new(0,0,255))
+    expect(docx.elements[1].properties.table_borders.right.color).to eq(OoxmlParser::Color.new(0,0,255))
+    expect(docx.elements[1].properties.table_borders.top.color).to eq(OoxmlParser::Color.new(0,0,255))
+    expect(docx.elements[1].properties.table_borders.inside_horizontal.color).to eq(OoxmlParser::Color.new(255,0,0))
+    expect(docx.elements[1].properties.table_borders.inside_vertical.color).to eq(OoxmlParser::Color.new(255,0,0))
   end
 end
