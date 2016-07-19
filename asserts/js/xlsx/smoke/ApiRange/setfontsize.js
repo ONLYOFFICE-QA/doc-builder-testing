dcbfb75d0f1b65a0b6050bcea9d7d6cb22a2e531
Dmitriy Rotatyy <kvazilife@gmail.com>
2016-07-19 09:46:48 +0300

    Add tests for basic functinal of range api method. Add js examples - i take it from help center

---
+++ asserts/js/xlsx/smoke/ApiRange/setfontsize.js
@@ -0,0 +1,7 @@
+builder.CreateFile("xlsx");
+var oWorksheet = Api.GetActiveSheet();
+oWorksheet.GetRange('A2').SetValue('2');
+var sRange = oWorksheet.GetRange("A1:D5");
+sRange.SetFontSize(20);
+builder.SaveFile("xlsx", "SetFontSize.xlsx");
+builder.CloseFile();
