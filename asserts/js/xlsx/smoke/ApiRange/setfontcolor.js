8f2a819e23e58e83d6dcc6d295b7a17686691b96
Dmitriy Rotatyy <kvazilife@gmail.com>
2016-07-19 09:46:48 +0300

    Add tests for basic functinal of  apiworksheet methods. Add js examples - i take it from help center
    edit comment

---
+++ asserts/js/xlsx/smoke/ApiRange/setfontcolor.js
@@ -0,0 +1,7 @@
+builder.CreateFile("xlsx");
+var oWorksheet = Api.GetActiveSheet();
+oWorksheet.GetRange('A2').SetValue('2');
+var sRange = oWorksheet.GetRange("A1:D5");
+sRange.SetFontColor(0, 255, 0);
+builder.SaveFile("xlsx", "SetFontColor.xlsx");
+builder.CloseFile();
