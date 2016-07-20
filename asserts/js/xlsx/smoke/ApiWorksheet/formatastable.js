21327ff96c95b187db0181e17d0c7f4df4358740
Dmitriy Rotatyy <kvazilife@gmail.com>
2016-07-20 17:54:07 +0300

    add tests for api worksheet api. I take all examples from help center and add expected from ever in spec

---
+++ asserts/js/xlsx/smoke/ApiWorksheet/formatastable.js
@@ -0,0 +1,5 @@
+builder.CreateFile("xlsx");
+var oWorksheet = Api.GetActiveSheet();
+oWorksheet.FormatAsTable("A1:E10");
+builder.SaveFile("xlsx", "FormatAsTable.xlsx");
+builder.CloseFile();
---
+++ asserts/js/xlsx/smoke/ApiWorksheet/setcolumnwidth.js
@@ -0,0 +1,6 @@
+builder.CreateFile("xlsx");
+var oWorksheet = Api.GetActiveSheet();
+oWorksheet.SetColumnWidth(0, 10);
+oWorksheet.SetColumnWidth(1, 20);
+builder.SaveFile("xlsx", "SetColumnWidth.xlsx");
+builder.CloseFile();
