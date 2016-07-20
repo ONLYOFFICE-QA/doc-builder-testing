3b7d207e9eef03bfee6c8fbfc57af2a000ea8e45
Dmitriy Rotatyy <kvazilife@gmail.com>
2016-07-20 17:45:49 +0300

    edit test

--- asserts/js/xlsx/smoke/ApiRange/setvalue.js
+++ asserts/js/xlsx/smoke/ApiRange/setvalue.js
@@ -4,4 +4,5 @@ oWorksheet.GetRange('B1').SetValue('2');
 oWorksheet.GetRange('B2').SetValue('2');
 oWorksheet.GetRange('A3').SetValue('2x2=');
 oWorksheet.GetRange('B3').SetValue('=B1*B2');
+builder.SaveFile("xlsx", "SetValue.xlsx");
 builder.CloseFile();
--- spec/xlsx/smoke/ApiRange_spec.rb
+++ spec/xlsx/smoke/ApiRange_spec.rb
@@ -45,9 +45,11 @@ describe 'ApiRange section tests' do
     end
   end
 
-  it 'SetValue | SetFontSize' do
-    pending 'Documentation error'
+  it 'ApiRange | SetValue' do
     xlsx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/xlsx/smoke/ApiRange/setvalue.js')
-    expect(xlsx.nil?).to eq(false)
+    expect(xlsx.worksheets.first.rows.first.cells[1].text).to eq('2')
+    expect(xlsx.worksheets.first.rows[1].cells[1].text).to eq('2')
+    expect(xlsx.worksheets.first.rows[2].cells[0].text).to eq('2x2=')
+    expect(xlsx.worksheets.first.rows[2].cells[1].text).to eq('4')
   end
 end
