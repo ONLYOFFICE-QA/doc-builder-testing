5ac55b55584d5081cf1d2042be11ea27a614177e
Dmitriy Rotatyy <kvazilife@gmail.com>
2016-07-25 17:53:31 +0300

    add smoke tests for ApiDrawing section from help center. Its test check basic functional of doc builder (doc creator). I get all examples from this this section and add to it some expects

--- spec/docx/smoke/ApiDrawing_spec.rb
+++ spec/docx/smoke/ApiDrawing_spec.rb
@@ -1,42 +1,55 @@
 require 'spec_helper'
 describe 'ApiDrawing section tests' do
+  before :all do
+    OoxmlParser.configure do |config|
+      config.units = :emu
+    end
+  end
   it 'ApiDrawing | GetClassType method' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/ApiDrawing/getclasstype.js')
-    expect(docx.nil?).to eq(false)
+    expect(docx.elements[1].character_style_array.first.text).to eq('Class Type = shape')
   end
 
   it 'ApiDrawing | SetDistances method' do
+    pending 'add functional to parser'
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/ApiDrawing/setdistances.js')
-    expect(docx.nil?).to eq(false)
+    expect(docx.elements.first.character_style_array[4].alternate_content.office2010_content).to eq(false)
+    expect(docx.elements.first.character_style_array[4].alternate_content.office2010_content.properties.wrap.distance_from_text).to eq(false)
   end
 
   it 'ApiDrawing | SetHorAlign method' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/ApiDrawing/sethoralign.js')
-    expect(docx.nil?).to eq(false)
+    expect(docx.elements.first.character_style_array[4].alternate_content.office2010_content.properties.horizontal_position.align).to eq(:center)
+    expect(docx.elements.first.character_style_array[4].alternate_content.office2010_content.properties.horizontal_position.relative_from).to eq(:page)
   end
 
   it 'ApiDrawing | SetHorPosition method' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/ApiDrawing/sethorposition.js')
-    expect(docx.nil?).to eq(false)
+    expect(docx.elements.first.character_style_array[4].alternate_content.office2010_content.properties.horizontal_position.offset).to eq(0)
+    expect(docx.elements.first.character_style_array[4].alternate_content.office2010_content.properties.horizontal_position.relative_from).to eq(:right_margin)
   end
 
   it 'ApiDrawing | SetSize method' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/ApiDrawing/setsize.js')
-    expect(docx.nil?).to eq(false)
+    expect(docx.elements.first.character_style_array[4].alternate_content.office2010_content.properties.object_size.x).to eq(1828800.0)
+    expect(docx.elements.first.character_style_array[4].alternate_content.office2010_content.properties.object_size.y).to eq(1828800.0)
   end
 
   it 'ApiDrawing | SetVerAlign method' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/ApiDrawing/setveralign.js')
-    expect(docx.nil?).to eq(false)
+    expect(docx.elements.first.character_style_array[4].alternate_content.office2010_content.properties.vertical_position.align).to eq(:top)
+    expect(docx.elements.first.character_style_array[4].alternate_content.office2010_content.properties.vertical_position.relative_from).to eq(:page)
   end
 
   it 'ApiDrawing | SetVerPosition method' do
+    pending 'parser cant use emu metric in this value'
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/ApiDrawing/setverposition.js')
-    expect(docx.nil?).to eq(false)
+    expect(docx.elements.first.character_style_array[4].alternate_content.office2010_content.properties.vertical_position.offset).to eq(914400)
+    expect(docx.elements.first.character_style_array[4].alternate_content.office2010_content.properties.vertical_position.relative_from).to eq(:page)
   end
 
   it 'ApiDrawing | SetWrappingStyle method' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/ApiDrawing/setwrappingstyle.js')
-    expect(docx.nil?).to eq(false)
+    expect(docx.elements.first.character_style_array[4].alternate_content.office2010_content.properties.wrap.wrap_text).to eq(:square)
   end
 end
