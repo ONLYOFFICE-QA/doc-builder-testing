809815e0dd3ca7023817894c691c89beeff62d99
Dmitriy Rotatyy <kvazilife@gmail.com>
2016-08-16 14:29:39 +0300

    add new module and put method for parse. It needle for separate methods for doc_builder wrapper and other(as example - for check file size after build). In future i want write more help-methods, and do it in wrapper is not correct rubocop detected for all files in lib dir

--- lib/doc_builder_testing/doc_builder_wrapper.rb
+++ lib/doc_builder_testing/doc_builder_wrapper.rb
@@ -1,7 +1,10 @@
 # Class for Wrapping doc building
 require 'tempfile'
 require 'ooxml_parser'
+require_relative 'doc_builder_helper'
 class DocBuilderWrapper
+  include DocBuilderHelper
+
   # Path to Builder exe
   attr_accessor :builder_exe
 
@@ -12,8 +15,8 @@ class DocBuilderWrapper
   def build_doc(script_file)
     build_result = `#{@builder_exe} #{script_file} 2>&1`
     if /[Ee]rror|not found/ === build_result
-     return if /licence error/ === build_result
-     raise DocBuilderError, build_result
+      return if /licence error/ === build_result
+      raise DocBuilderError, build_result
     end
   end
 
@@ -23,7 +26,7 @@ class DocBuilderWrapper
   def build_doc_and_parse(script_file)
     temp_script_data = DocBuilderWrapper.change_output_file(script_file)
     build_doc(temp_script_data[:temp_script_file])
-    OoxmlParser::Parser.parse(temp_script_data[:temp_output_file])
+    parse(temp_script_data[:temp_output_file])
   end
 
   # Make a copy of script file, so no need to change output path on real file
@@ -31,14 +34,14 @@ class DocBuilderWrapper
   # @param format [Symbol, String] type of file (docx, xlsx)
   # @return [Hash] {temp_script_file: file_path, temp_output_file: output_path}
   def self.change_output_file(script_file)
-    script_file_content = File.open(script_file, "r").read
+    script_file_content = File.open(script_file, 'r').read
     output_format = DocBuilderWrapper.recognize_output_format(script_file_content)
     temp_output_file = Tempfile.new([File.basename(script_file), ".#{output_format}"])
     script_file_content.gsub!(/^builder\.SaveFile.*$/, "builder.SaveFile(\"#{output_format}\", \"#{temp_output_file.path}\");")
     temp_script_file = Tempfile.new([File.basename(script_file), File.extname(script_file)])
     temp_script_file.write(script_file_content)
     temp_script_file.close
-    {temp_script_file: temp_script_file.path, temp_output_file: temp_output_file.path}
+    { temp_script_file: temp_script_file.path, temp_output_file: temp_output_file.path }
   end
 
   # Recognize format from script file
