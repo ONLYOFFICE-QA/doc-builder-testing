d84a13327fd5f214128db346b5cdc7b4e02eb9ee
Pavel Lobashov <shockwavenn@gmail.com>
2016-08-18 16:07:53 +0300

    Fix RuboCop `Style/IndentationWidth` cop

--- spec/docx/smoke/ApiTableRowPr_spec.rb
+++ spec/docx/smoke/ApiTableRowPr_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 describe 'ApiTableRowPr section tests' do
- it 'ApiTableRowPr | GetClassType method' do
+  it 'ApiTableRowPr | GetClassType method' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/ApiTableRowPr/getclasstype.js')
     expect(docx.nil?).to eq(false)
- end
+  end
 
- it 'ApiTableRowPr | SetHeight method' do
+  it 'ApiTableRowPr | SetHeight method' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/ApiTableRowPr/setheight.js')
     expect(docx.nil?).to eq(false)
- end
+  end
 
- it 'ApiTableRowPr | SetTableHeader method' do
+  it 'ApiTableRowPr | SetTableHeader method' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/ApiTableRowPr/settableheader.js')
     expect(docx.nil?).to eq(false)
- end
+  end
 end
