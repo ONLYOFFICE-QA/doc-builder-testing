47f40ca087410271ec1674e7dba24c38e1511997
Dmitriy Rotatyy <kvazilife@gmail.com>
2016-08-18 21:48:34 +0300

    add tests for fdr create (from docx)

---
+++ asserts/js/other_formats/createblipfill-pdf.js
@@ -0,0 +1,10 @@
+builder.CreateFile("docx");
+var oDocument = Api.GetDocument();
+var oFill, oStroke, oDrawing, oParagraph;
+oParagraph = oDocument.GetElement(0);
+oFill = Api.CreateBlipFill("http://helpcenter.onlyoffice.com/images/icons/icon_DocumentEditors.png", "tile");
+oStroke = Api.CreateStroke(0, Api.CreateNoFill());
+oDrawing = Api.CreateShape("star10", 5930900, 595605, oFill, oStroke);
+oParagraph.AddDrawing(oDrawing);
+builder.SaveFile("pdf", "CreateBlipFill.pdf");
+builder.CloseFile();
--- spec/all_formats_smoke_spec_spec.rb
+++ spec/all_formats_smoke_spec_spec.rb
@@ -8,13 +8,18 @@ describe 'All formats tests' do
 
   describe 'Docx' do
     it 'All formats | docx' do
-      docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/smoke/Api/createblipfill.js')
+      docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/other_formats/createblipfill.js')
       expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.type).to eq(:shape)
       expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.preset_geometry.name).to eq(:star10)
       expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.fill_color.type).to eq(:picture)
       expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.fill_color.value.nil?).to be_falsey
       expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.shape_size.extent.x).to eq(5_930_900.0)
       expect(docx.elements.first.character_style_array[1].alternate_content.office2010_content.graphic.data.properties.shape_size.extent.y).to eq(595_605.0)
+    end
+
+    it 'All formats | pdf' do
+      pdf = DocBuilderWrapper.new.build_doc_without_parse('asserts/js/other_formats/createblipfill-pdf.js')
+      p
     end
   end
 end
