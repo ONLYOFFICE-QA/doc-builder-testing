723342bd318c79f6df4e32d5281d8295e0a972b9
Pavel Lobashov <shockwavenn@gmail.com>
2016-08-29 18:06:28 +0300

    Fix verification for new table cell margins
    Since https://github.com/ONLYOFFICE/ooxml_parser/pull/164

--- spec/docx/paragraph/tables/table_with_cell_margins_spec.rb
+++ spec/docx/paragraph/tables/table_with_cell_margins_spec.rb
@@ -4,8 +4,8 @@ describe 'Add table with cells margins' do
   it 'Table with cell margins: bottom, left, top' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/paragraph/tables/table_with_cell_margins.js')
     expect(docx.elements[1].rows.length).to eq(3)
-    expect(docx.elements[1].rows.first.cells[1].properties.margins.bottom).to eq(0.53)
-    expect(docx.elements[1].rows.first.cells[1].properties.margins.left).to eq(0.18)
-    expect(docx.elements[1].rows.first.cells[1].properties.margins.top).to eq(0.71)
+    expect(docx.elements[1].rows.first.cells[1].properties.table_cell_margin.bottom).to eq(OoxmlParser::OoxmlSize.new(0.52, :centimeter))
+    expect(docx.elements[1].rows.first.cells[1].properties.table_cell_margin.left).to eq(OoxmlParser::OoxmlSize.new(0.17, :centimeter))
+    expect(docx.elements[1].rows.first.cells[1].properties.table_cell_margin.top).to eq(OoxmlParser::OoxmlSize.new(0.7, :centimeter))
   end
 end
--- spec/docx/paragraph/tables/table_with_margins_spec.rb
+++ spec/docx/paragraph/tables/table_with_margins_spec.rb
@@ -5,9 +5,9 @@ describe 'Add table with margins' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/paragraph/tables/table_with_margins.js')
     expect(docx.elements[1].rows.length).to eq(3)
     expect(docx.elements[1].rows.first.cells.length).to eq(3)
-    expect(docx.elements[1].rows.first.cells.first.properties.margins.bottom).to eq(0.127)
-    expect(docx.elements[1].rows.first.cells.first.properties.margins.top).to eq(0.127)
-    expect(docx.elements[1].rows.first.cells.first.properties.margins.left).to eq(0.254)
-    expect(docx.elements[1].rows.first.cells.first.properties.margins.right).to eq(0.254)
+    expect(docx.elements[1].properties.table_cell_margin.bottom).to eq(OoxmlParser::OoxmlSize.new(0.35, :centimeter))
+    expect(docx.elements[1].properties.table_cell_margin.top).to eq(OoxmlParser::OoxmlSize.new(0.18, :centimeter))
+    expect(docx.elements[1].properties.table_cell_margin.left).to eq(OoxmlParser::OoxmlSize.new(0.7, :centimeter))
+    expect(docx.elements[1].properties.table_cell_margin.right).to eq(OoxmlParser::OoxmlSize.new(0.35, :centimeter))
   end
 end
