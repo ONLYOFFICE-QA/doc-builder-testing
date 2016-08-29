1e15ea38fd1deb1b55850eae2c9fc3b77ef1c8d2
Pavel Lobashov <shockwavenn@gmail.com>
2016-08-29 14:39:18 +0300

    Update test for table row height
    According with https://github.com/ONLYOFFICE/ooxml_parser/pull/163

--- spec/docx/paragraph/tables/table_with_row_height_spec.rb
+++ spec/docx/paragraph/tables/table_with_row_height_spec.rb
@@ -5,8 +5,8 @@ describe 'Add table custom rows height' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/paragraph/tables/table_with_row_height.js')
     expect(docx.elements[1].rows.length).to eq(3)
     expect(docx.elements[1].rows.first.cells.length).to eq(3)
-    expect(docx.elements[1].rows.first.table_row_properties.height).to eq(nil)
-    expect(docx.elements[1].rows[1].table_row_properties.height).to eq(1000.0)
-    expect(docx.elements[1].rows[2].table_row_properties.height).to eq(2000.0)
+    expect(docx.elements[1].rows.first.table_row_properties.height).to be_nil
+    expect(docx.elements[1].rows[1].table_row_properties.height.value).to eq(OoxmlParser::OoxmlSize.new(1000))
+    expect(docx.elements[1].rows[2].table_row_properties.height.value).to eq(OoxmlParser::OoxmlSize.new(2000))
   end
 end
