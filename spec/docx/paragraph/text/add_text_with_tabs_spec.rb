e6f34a06a9fc0625d1a9d89bd544f8620aa6017f
Pavel Lobashov <shockwavenn@gmail.com>
2016-09-06 11:06:12 +0300

    Redone some tabs placing
    In https://github.com/ONLYOFFICE/ooxml_parser/pull/207

--- spec/docx/paragraph/text/add_text_with_tabs_spec.rb
+++ spec/docx/paragraph/text/add_text_with_tabs_spec.rb
@@ -4,11 +4,11 @@ describe 'text with tabs' do
   it 'Tabs' do
     docx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/docx/paragraph/text/add_text_with_tabs.js')
     expect(docx.elements[1].character_style_array.first.text).to eq('Tabs')
-    expect(docx.elements[1].tabs.first.position).to eq(1.76)
-    expect(docx.elements[1].tabs.first.align).to eq(:center)
-    expect(docx.elements[1].tabs[1].position).to eq(2.65)
-    expect(docx.elements[1].tabs[1].align).to eq(:left)
-    expect(docx.elements[1].tabs[2].position).to eq(5.29)
-    expect(docx.elements[1].tabs[2].align).to eq(:right)
+    expect(docx.elements[1].paragraph_properties.tabs[0].position).to eq(OoxmlParser::OoxmlSize.new(1.76, :centimeter))
+    expect(docx.elements[1].paragraph_properties.tabs[0].align).to eq(:center)
+    expect(docx.elements[1].paragraph_properties.tabs[1].position).to eq(OoxmlParser::OoxmlSize.new(2.65, :centimeter))
+    expect(docx.elements[1].paragraph_properties.tabs[1].align).to eq(:left)
+    expect(docx.elements[1].paragraph_properties.tabs[2].position).to eq(OoxmlParser::OoxmlSize.new(5.29, :centimeter))
+    expect(docx.elements[1].paragraph_properties.tabs[2].align).to eq(:right)
   end
 end
