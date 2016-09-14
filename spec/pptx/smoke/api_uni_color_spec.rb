cbe33705332b57572ad1ebe0c288c339395858c2
Oleg Nazarov <nazarov90@gmail.com>
2016-09-14 18:07:23 +0300

    add test ApiUniColor

--- spec/pptx/smoke/api_uni_color_spec.rb
+++ spec/pptx/smoke/api_uni_color_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 describe 'ApiUniColor section tests' do
   it 'ApiUniColor | GetClassType method' do
     pptx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/pptx/smoke/api_uni_color/get_class_type.js')
-    expect(pptx).to be_with_data
+    expect(pptx.slides.first.elements.first.text_body.paragraphs.first.runs.first.text).to eq('Class Type = presetColor')
   end
 end
