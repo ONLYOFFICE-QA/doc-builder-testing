76032d8b0e32be6efe83d3e0c570e3f8ad74d2e8
Anna Vybornova <gingetsu2007@yandex.ru>
2016-09-15 18:18:06 +0300

    Add bullet tests for presentation.

--- spec/pptx/smoke/api_bullet_spec.rb
+++ spec/pptx/smoke/api_bullet_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 describe 'ApiBullet section tests' do
   it 'ApiBullet | GetClassType method' do
     pptx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/pptx/smoke/api_bullet/get_class_type.js')
-    expect(pptx).to be_with_data
+    expect(pptx.slides.first.elements.first.text_body.paragraphs.first.properties.numbering.symbol).to eq('-')
+    expect(pptx.slides[0].elements.first.text_body.paragraphs.last.characters.first.text).to eq('Class Type = bullet')
   end
 end
