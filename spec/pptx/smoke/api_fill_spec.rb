8830e195d087481f1605d84ba1eec1783caaced9
Anna Vybornova <gingetsu2007@yandex.ru>
2016-09-15 18:23:29 +0300

    Add fill tests for presentation.

--- spec/pptx/smoke/api_fill_spec.rb
+++ spec/pptx/smoke/api_fill_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 describe 'ApiFill section tests' do
   it 'ApiFill | GetClassType method' do
     pptx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/pptx/smoke/api_fill/get_class_type.js')
-    expect(pptx).to be_with_data
+    expect(pptx.slides[0].elements.first.text_body.paragraphs.last.characters.first.text).to eq('Class Type = fill')
   end
 end
