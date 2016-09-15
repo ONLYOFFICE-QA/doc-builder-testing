9e40d5d3eb8726455dff7867d1402f9d49552f85
Oleg Nazarov <nazarov90@gmail.com>
2016-09-15 12:14:05 +0300

    add test ApiStroke

--- spec/pptx/smoke/api_stroke_spec.rb
+++ spec/pptx/smoke/api_stroke_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 describe 'ApiStroke section tests' do
   it 'ApiStroke | GetClassType method' do
     pptx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/pptx/smoke/api_stroke/get_class_type.js')
-    expect(pptx).to be_with_data
+    expect(pptx.slides.first.elements.first.text_body.paragraphs.first.runs.first.text).to eq('Class Type = stroke')
   end
 end
