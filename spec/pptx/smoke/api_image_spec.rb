37580a80f70d5bed25eacae752c91fe352492665
Anna Vybornova <gingetsu2007@yandex.ru>
2016-09-15 18:31:37 +0300

    Add image test for presentation.

--- spec/pptx/smoke/api_image_spec.rb
+++ spec/pptx/smoke/api_image_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 describe 'ApiImage section tests' do
   it 'ApiImage | GetClassType method' do
-    pending('https://github.com/ONLYOFFICE/ooxml_parser/issues/254')
     pptx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/pptx/smoke/api_image/get_class_type.js')
-    expect(pptx).to be_with_data
+    expect(pptx.slides[0].elements.last.text_body.paragraphs.last.characters.first.text).to eq('Class Type = image')
   end
 end
