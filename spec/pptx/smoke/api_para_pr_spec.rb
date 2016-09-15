82b4481b708e043ba1dfea8d992315a4da0bcca6
Anna Vybornova <gingetsu2007@yandex.ru>
2016-09-15 18:24:52 +0300

    Add para_pr tests for presentation.

--- spec/pptx/smoke/api_para_pr_spec.rb
+++ spec/pptx/smoke/api_para_pr_spec.rb
@@ -2,51 +2,56 @@ require 'spec_helper'
 describe 'ApiParaPr section tests' do
   it 'ApiParaPr | GetClassType method' do
     pptx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/pptx/smoke/api_para_pr/get_class_type.js')
-    expect(pptx).to be_with_data
+    expect(pptx.slides[0].elements.first.text_body.paragraphs.last.characters.first.text).to eq('Class Type = paraPr')
   end
 
   it 'ApiParaPr | SetBullet method' do
     pptx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/pptx/smoke/api_para_pr/set_bullet.js')
-    expect(pptx).to be_with_data
+    expect(pptx.slides.first.elements.first.text_body.paragraphs.first.properties.numbering.symbol).to eq('-')
   end
 
   it 'ApiParaPr | SetIndFirstLine method' do
     pptx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/pptx/smoke/api_para_pr/set_ind_first_line.js')
-    expect(pptx).to be_with_data
+    expect(pptx.slides.first.elements.first.text_body.paragraphs.first.properties.indent).to eq(OoxmlParser::OoxmlSize.new(1440, :twip))
   end
 
   it 'ApiParaPr | SetIndLeft method' do
     pptx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/pptx/smoke/api_para_pr/set_ind_left.js')
-    expect(pptx).to be_with_data
+    expect(pptx.slides.first.elements.first.text_body.paragraphs.first.properties.margin_left).to eq(OoxmlParser::OoxmlSize.new(2880, :twip))
   end
 
   it 'ApiParaPr | SetIndRight method' do
     pptx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/pptx/smoke/api_para_pr/set_ind_right.js')
-    expect(pptx).to be_with_data
+    expect(pptx.slides.first.elements.first.text_body.paragraphs.first.properties.margin_right).to eq(OoxmlParser::OoxmlSize.new(2880, :twip))
   end
 
   it 'ApiParaPr | SetJc method' do
     pptx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/pptx/smoke/api_para_pr/set_jc.js')
-    expect(pptx).to be_with_data
+    expect(pptx.slides.first.elements.first.text_body.paragraphs.first.properties.align).to eq(:center)
   end
 
   it 'ApiParaPr | SetSpacingAfter method' do
     pptx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/pptx/smoke/api_para_pr/set_spacing_after.js')
-    expect(pptx).to be_with_data
+    expect(pptx.slides.first.elements.first.text_body.paragraphs.first.properties.spacing.after).to eq(OoxmlParser::OoxmlSize.new(1440, :twip))
   end
 
   it 'ApiParaPr | SetSpacingBefore method' do
     pptx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/pptx/smoke/api_para_pr/set_spacing_before.js')
-    expect(pptx).to be_with_data
+    expect(pptx.slides.first.elements.first.text_body.paragraphs.last.properties.spacing.before).to eq(OoxmlParser::OoxmlSize.new(1440, :twip))
   end
 
   it 'ApiParaPr | SetSpacingLine method' do
     pptx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/pptx/smoke/api_para_pr/set_spacing_line.js')
-    expect(pptx).to be_with_data
+    expect(pptx.slides.first.elements.first.text_body.paragraphs.first.properties.spacing.line).to eq(OoxmlParser::OoxmlSize.new(3*240, :one_1000th_percent))
   end
 
   it 'ApiParaPr | SetTabs method' do
     pptx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/pptx/smoke/api_para_pr/set_tabs.js')
-    expect(pptx).to be_with_data
+    expect(pptx.slides.first.elements.first.text_body.paragraphs.first.properties.tabs[0].align).to eq(:left)
+    expect(pptx.slides.first.elements.first.text_body.paragraphs.first.properties.tabs[0].position).to eq(OoxmlParser::OoxmlSize.new(1440, :twip))
+    expect(pptx.slides.first.elements.first.text_body.paragraphs.first.properties.tabs[1].align).to eq(:center)
+    expect(pptx.slides.first.elements.first.text_body.paragraphs.first.properties.tabs[1].position).to eq(OoxmlParser::OoxmlSize.new(4320, :twip))
+    expect(pptx.slides.first.elements.first.text_body.paragraphs.first.properties.tabs[2].align).to eq(:right)
+    expect(pptx.slides.first.elements.first.text_body.paragraphs.first.properties.tabs[2].position).to eq(OoxmlParser::OoxmlSize.new(7200, :twip))
   end
 end
