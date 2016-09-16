e1e498b057749f32c0790b0e5bc46f43ba4252f5
Oleg Nazarov <nazarov90@gmail.com>
2016-09-16 12:37:02 +0300

    add ApiPresetColor tests for presentation

--- spec/pptx/smoke/api_preset_color_spec.rb
+++ spec/pptx/smoke/api_preset_color_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 describe 'ApiPresetColor section tests' do
   it 'ApiPresetColor | GetClassType method' do
     pptx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/pptx/smoke/api_preset_color/get_class_type.js')
-    expect(pptx).to be_with_data
+    expect(pptx.slides.first.elements.first.text_body.paragraphs.first.runs.first.text).to eq('Class Type = presetColor')
   end
 end
