0058251611cdad9a8d2218810683d8eaa7364efe
Oleg Nazarov <nazarov90@gmail.com>
2016-09-16 12:35:20 +0300

    add ApiSchemeColor tests for presentation

--- spec/pptx/smoke/api_scheme_color_spec.rb
+++ spec/pptx/smoke/api_scheme_color_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 describe 'ApiSchemeColor section tests' do
   it 'ApiSchemeColor | GetClassType method' do
     pptx = DocBuilderWrapper.new.build_doc_and_parse('asserts/js/pptx/smoke/api_scheme_color/get_class_type.js')
-    expect(pptx.slides.first.elements.first.shape_properties.preset.name).to eq(:flowChartMagneticTape)
     expect(pptx.slides[0].nonempty_elements.first.text_body.paragraphs.first.characters.first.text).to eq('Class Type = schemeColor')
   end
 end
