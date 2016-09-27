0c584021d6cd71d3db9792cc4d2de32a283faf99
flaminestome <flamine@list.ru>
2016-09-27 17:25:44 +0300

    add opportunity to use js files with file editing. Add test to edit custom file. Add custom file

---
+++ asserts/js/docx/functional/edit_document_only_paragraph.js
@@ -0,0 +1,8 @@
+builder.OpenFile("asserts/sample_files/only_paragraph.docx");
+var oDocument = Api.GetDocument();
+var oParagraph;
+oParagraph = oDocument.GetElement(0);
+oParagraph.SetJc("center");
+oParagraph.AddText("Center");
+builder.SaveFile("docx", "only_paragraph.docx");
+builder.CloseFile();
---
+++ asserts/js/docx/functional/edit_document_only_paragraph_change_color.js
@@ -0,0 +1,8 @@
+builder.OpenFile("asserts/sample_files/only_paragraph.docx");
+var oDocument = Api.GetDocument();
+var oParagraph;
+oParagraph = oDocument.GetElement(0);
+oRun = oParagraph.GetElement(0);
+oRun.SetColor(0, 0, 255);
+builder.SaveFile("docx", "only_paragraph.docx");
+builder.CloseFile();
