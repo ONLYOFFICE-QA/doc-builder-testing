e316f8f48a4b797b8067fa68543863eccff480f4
Pavel Lobashov <shockwavenn@gmail.com>
2017-10-20 10:41:45 +0300

    Rename js files and folder for common scheme

--- spec/docx/smoke/api_uni_color_spec.rb
+++ spec/docx/smoke/api_uni_color_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 describe 'ApiUniColor section tests' do
   it 'ApiUniColor | GetClassType method' do
-    docx = builder.build_doc_and_parse('asserts/js/docx/smoke/ApiUniColor/getclasstype.js')
+    docx = builder.build_doc_and_parse('asserts/js/docx/smoke/api_uni_color/get_class_type.js')
     expect(docx.elements[1].nonempty_runs.first.text).to eq('Class Type = presetColor')
   end
 end
