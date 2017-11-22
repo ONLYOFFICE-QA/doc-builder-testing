de9733f592221e0c91b73c166c81e8657b74d277
Pavel Lobashov <shockwavenn@gmail.com>
2017-11-22 13:04:06 +0300

    Update `asserts/js/docx/smoke/api/get_document.js`
    Documentation was added in
    https://github.com/ONLYOFFICE/api.onlyoffice.com/commit/42b9ef0e3e6b33da0ac3c9cee13c2cf2f9b14821

--- asserts/js/docx/smoke/api/get_document.js
+++ asserts/js/docx/smoke/api/get_document.js
@@ -1,1 +1,8 @@
+builder.CreateFile("docx");
 var oDocument = Api.GetDocument();
+var oParagraph;
+oParagraph = Api.CreateParagraph();
+oParagraph.AddText("This is a new paragraph");
+oDocument.Push(oParagraph);
+builder.SaveFile("docx", "GetDocument.docx");
+builder.CloseFile();
