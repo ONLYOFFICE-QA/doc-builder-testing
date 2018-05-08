07c16f06700c61e34e8813728401da2127fdc1ce
flaminestone <flamine@list.ru>
2018-05-08 14:47:27 +0300

    Add exception if response urls is empty

--- lib/doc_builder_testing/exceptions.rb
+++ lib/doc_builder_testing/exceptions.rb
@@ -1,3 +1,4 @@
 class DocBuilderError < StandardError; end
 class WebDocBuilderError < StandardError; end
+class EmptyUrlsInWebBuilderResponse < WebDocBuilderError; end
 class DocBuilderFileCreationError < StandardError; end
