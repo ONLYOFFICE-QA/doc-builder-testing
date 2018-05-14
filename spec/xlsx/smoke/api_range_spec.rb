255d0bb8104b75b70c62f8f56bc7d3d45f24a814
flaminestone <flamine@list.ru>
2018-05-14 17:18:04 +0300

    Add test for attribute Width in ApiRange. Add pending to it https://bugzilla.onlyoffice.com/show_bug.cgi?id=37730

--- spec/xlsx/smoke/api_range_spec.rb
+++ spec/xlsx/smoke/api_range_spec.rb
@@ -318,4 +318,11 @@ describe 'ApiRange section tests' do
     expect(xlsx.worksheets.first.rows[0].cells[0].style.fill_color.pattern_fill.background_color).to eq(OoxmlParser::Color.new(255, 224, 204))
     expect(xlsx.worksheets.first.rows[1].cells[0].style.fill_color.pattern_fill.background_color).to eq(OoxmlParser::Color.new(255, 224, 204))
   end
+
+  it 'ApiRange | Width' do
+    pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=37730'
+    # skip if builder.semver < Semantic::Version.new('5.2.0')
+    xlsx = builder.build_doc_and_parse('asserts/js/xlsx/smoke/api_range/width_getter.js')
+    expect(xlsx.worksheets.first.rows.first.cells.first.text).to eq('15')
+  end
 end
