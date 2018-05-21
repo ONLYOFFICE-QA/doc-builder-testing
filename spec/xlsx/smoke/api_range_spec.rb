9638b8f541d65526dddf7cae22abf49e7ff19124
flaminestone <flamine@list.ru>
2018-05-21 21:11:34 +0300

    Add test for SetWrapText method

--- spec/xlsx/smoke/api_range_spec.rb
+++ spec/xlsx/smoke/api_range_spec.rb
@@ -256,6 +256,12 @@ describe 'ApiRange section tests' do
     expect(xlsx.worksheets.first.rows[1].cells[2].style.alignment.wrap_text).to be_falsey
   end
 
+  it 'ApiRange | SetWrapText method' do
+    xlsx = builder.build_doc_and_parse('asserts/js/xlsx/smoke/api_range/set_wrap_text.js')
+    expect(xlsx.worksheets.first.rows[1].cells[0].style.alignment.wrap_text).to be_truthy
+    expect(xlsx.worksheets.first.rows[1].cells[2].style.alignment.wrap_text).to be_falsey
+  end
+
   it 'ApiRange | SetWrap method' do
     skip if builder.semver < Semantic::Version.new('5.2.0')
     xlsx = builder.build_doc_and_parse('asserts/js/xlsx/smoke/api_range/setter_wrap.js')
