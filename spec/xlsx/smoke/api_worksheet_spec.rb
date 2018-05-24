0fa8a7b6493dd982c44bd3de8690295fc1707feb
flaminestone <flamine@list.ru>
2018-05-24 17:32:59 +0300

    Add test for PageOrientation property

--- spec/xlsx/smoke/api_worksheet_spec.rb
+++ spec/xlsx/smoke/api_worksheet_spec.rb
@@ -232,15 +232,15 @@ describe 'ApiWorksheet section tests' do
     expect(xlsx.worksheets.first.rows.first.cells.first.text).to eq('xlPortrait')
   end
 
-  it 'ApiWorksheet | GetPageOrientation method' do
-    skip if builder.semver < Semantic::Version.new('5.2.0')
-    xlsx = builder.build_and_parse('asserts/js/xlsx/smoke/api_worksheet/get_page_orientation.js')
-    expect(xlsx.worksheets.first.rows.first.cells.first.text).to eq('xlPortrait')
-  end
-
   it 'ApiWorksheet | SetPageOrientation method' do
     skip if builder.semver < Semantic::Version.new('5.2.0')
     pdf = builder.build_and_parse('asserts/js/xlsx/smoke/api_worksheet/set_page_orientation.js')
+    expect(pdf.page_size).to eq('Landscape A4')
+  end
+
+  it 'ApiWorksheet | PageOrientation property' do
+    skip if builder.semver < Semantic::Version.new('5.2.0')
+    pdf = builder.build_and_parse('asserts/js/xlsx/smoke/api_worksheet/page_orientation_property.js')
     expect(pdf.page_size).to eq('Landscape A4')
   end
 end
