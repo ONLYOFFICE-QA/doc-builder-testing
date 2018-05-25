7e3dcdec5d86a6db3c04b79442f1da908181ec1d
Pavel Lobashov <shockwavenn@gmail.com>
2018-05-25 15:18:49 +0300

    Add test for checing Api.CreateImage with base64 image

---
+++ spec/pptx/shape/image_in_shape_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+describe 'Image in shape' do
+  it 'ApiChart | GetClassType method' do
+    pptx = builder.build_and_parse('asserts/js/pptx/shape/create_image_base64.js')
+    expect(pptx.slides.first.elements.first.path_to_image.file_reference.content.length).to be >= 1_000
+    expect(pptx.slides.first.elements.first.properties.shape_size.extent.x).to eq(OoxmlParser::OoxmlSize.new(300 * 36_000, :emu))
+    expect(pptx.slides.first.elements.first.properties.shape_size.extent.y).to eq(OoxmlParser::OoxmlSize.new(150 * 36_000, :emu))
+  end
+end
