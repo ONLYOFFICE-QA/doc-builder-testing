ef2b5913272fd9ff964a87abd6062b50a2230836
flaminestone <flamine@list.ru>
2018-05-29 17:54:13 +0300

    Add test for GetRow() Method

--- spec/pptx/smoke/api_table_spec.rb
+++ spec/pptx/smoke/api_table_spec.rb
@@ -4,4 +4,10 @@ describe 'Api Table section tests' do
     pptx = builder.build_and_parse('asserts/js/pptx/smoke/api_table/get_class_type.js')
     expect(pptx.slides.first.elements.last.graphic_data.first.rows.first.cells.first.text_body.paragraphs[0].runs.first.text).to eq('table')
   end
+
+  it 'Api | GetRow method' do
+    pptx = builder.build_and_parse('asserts/js/pptx/smoke/api_table/get_row.js')
+    expect(pptx.slides.first.elements.last.graphic_data.first.rows.first.cells.first.text_body.paragraphs[0].runs.first.text).to eq('row1')
+    expect(pptx.slides.first.elements.last.graphic_data.first.rows[2].cells.first.text_body.paragraphs[0].runs.first.text).to eq('row2')
+  end
 end
