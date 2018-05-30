a3424c7929101be1c1e4d57efa87601c34115a13
flaminestone <flamine@list.ru>
2018-05-30 18:35:38 +0300

    Add test for GetContent() method for Table Cell

--- spec/pptx/smoke/api_table_cell_spec.rb
+++ spec/pptx/smoke/api_table_cell_spec.rb
@@ -4,4 +4,10 @@ describe 'Api Table Cell section tests' do
     pptx = builder.build_and_parse('asserts/js/pptx/smoke/api_table_cell/get_class_type.js')
     expect(pptx.slides.first.elements.last.graphic_data.first.rows.first.cells.first.text_body.paragraphs[0].runs.first.text).to eq('tableCell')
   end
+
+  it 'Api | GetContent method' do
+    pptx = builder.build_and_parse('asserts/js/pptx/smoke/api_table_cell/get_content.js')
+    expect(pptx.slides.first.elements.last.graphic_data.first.rows.first.cells.first.text_body.paragraphs[0].runs.first.text).to eq('cell1')
+    expect(pptx.slides.first.elements.last.graphic_data.first.rows.first.cells[1].text_body.paragraphs[0].runs.first.text).to eq('cell2')
+  end
 end
