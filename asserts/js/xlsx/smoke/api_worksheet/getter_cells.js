18fd847eccbbabf5fc0a2f1e1c572ab2ebdcd34f
flaminestone <flamine@list.ru>
2018-07-16 13:12:25 +0300

    Fix script because WrapText method will be added in 5.2.0

--- asserts/js/xlsx/smoke/api_worksheet/getter_cells.js
+++ asserts/js/xlsx/smoke/api_worksheet/getter_cells.js
@@ -1,5 +1,5 @@
 builder.CreateFile("xlsx");
 var oWorksheet = Api.GetActiveSheet();
-oWorksheet.Cells.WrapText = true;
+oWorksheet.Cells.SetFillColor(Api.CreateColorFromRGB(255, 224, 204));
 builder.SaveFile("xlsx", "GetterCells.xlsx");
 builder.CloseFile();
