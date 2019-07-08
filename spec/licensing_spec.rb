3dc805f366c6fff2e38402aa1296aa60b74f8065
Pavel Lobashov <shockwavenn@gmail.com>
2019-07-08 11:25:55 +0300

    Skip test for licensing
    In newer version of DocumentBuilder check
    for license is removed
    It's totally undocummented and change from version
    to version.
    I DON'T WANT to keep track of this feature on
    regular basis.
    May be removed after we release free version, but for now
    just skip it.

--- spec/licensing_spec.rb
+++ spec/licensing_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'DocumentBuilder licensing' do
   it 'running docbuilder create temp license' do
     skip if ENV['BUILDER_PLATFORM'] == 'WEB'
+    skip('License terms are too different for different versions')
     expect(Dir["#{builder.license_path}/*.lickey"].length).to eq(1)
   end
 end
