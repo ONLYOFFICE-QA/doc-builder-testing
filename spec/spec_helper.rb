58db1eff9bb5a7bad4affa68e6c2c4833f3e40a4
Pavel Lobashov <shockwavenn@gmail.com>
2020-08-06 14:38:47 +0300

    Fix rubocop `Style/RedundantFileExtensionInRequire` cop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@
 Encoding.default_external = Encoding::UTF_8
 require 'bundler/setup'
 require 'rspec'
-require_relative '../lib/doc_builder_testing.rb'
+require_relative '../lib/doc_builder_testing'
 
 def builder
   @builder ||= if ENV['BUILDER_PLATFORM'] == 'WEB'
