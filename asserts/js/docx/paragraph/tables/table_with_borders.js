6089eee2193e8486931e473f2117f348ab08cc36
Pavel Lobashov <ShockwaveNN@gmail.com>
2020-10-02 17:22:45 +0300

    Fix table with borders test (#418)
    Old variant check for properties of cell, but settings are set
    to table itself.
    It cause problem if no default table cells is defined
    (see https://github.com/ONLYOFFICE/ooxml_parser/issues/661#issuecomment-702733751
    for more details)
    Change test logic to be correct

--- asserts/js/docx/paragraph/tables/table_with_borders.js
+++ asserts/js/docx/paragraph/tables/table_with_borders.js
@@ -3,12 +3,12 @@ var oDocument = Api.GetDocument();
 oDocument.CreateNewHistoryPoint();
 var oTable = Api.CreateTable(3, 3);
 oDocument.Push(oTable);
-oTable.SetTableBorderTop("single", 32, 0, 0, 0, 0);
-oTable.SetTableBorderBottom("single", 32, 0, 0, 0, 0);
-oTable.SetTableBorderLeft("single", 32, 0, 0, 0, 0);
-oTable.SetTableBorderRight("single", 32, 0, 0, 0, 0);
-oTable.SetTableBorderInsideV("single", 32, 0, 0, 0, 0);
-oTable.SetTableBorderInsideH("single", 32, 0, 0, 0, 0);
+oTable.SetTableBorderTop("single", 8, 0, 1, 2, 3);
+oTable.SetTableBorderBottom("single", 16, 0, 4, 5, 6);
+oTable.SetTableBorderLeft("single", 24, 0, 7, 8, 9);
+oTable.SetTableBorderRight("single", 32, 0, 10, 11, 12);
+oTable.SetTableBorderInsideV("single", 40, 0, 13, 14, 15);
+oTable.SetTableBorderInsideH("single", 48, 0, 16, 17, 18);
 builder.SaveFile("docx", "table_with_borders.docx");
 builder.CloseFile();
 
