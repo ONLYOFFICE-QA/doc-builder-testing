fb324b38ae5ed0df93e6df786c2f26d2cec1121d
Pavel Lobashov <shockwavenn@gmail.com>
2021-06-30 15:44:32 +0300

    Actualize rubocop fixes

--- spec/pptx/smoke/api_paragraph_spec.rb
+++ spec/pptx/smoke/api_paragraph_spec.rb
@@ -63,8 +63,8 @@ describe 'ApiParagraph section tests' do
                .text_body.paragraphs
                .first.runs[2].text)
       .not_to eq('This is the third paragraph element '\
-                   '(it will be removed from the paragraph '\
-                   'and we will not see it). ')
+                 '(it will be removed from the paragraph '\
+                 'and we will not see it). ')
   end
 
   it 'ApiParagraph | SetBullet method' do
