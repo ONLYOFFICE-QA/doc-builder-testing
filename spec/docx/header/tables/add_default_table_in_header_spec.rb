21e56cc4727f3a15da7318bfa1cd77f14f62be89
Pavel Lobashov <shockwavenn@gmail.com>
2021-09-22 11:41:29 +0300

    Fix rubocop new issues

--- spec/docx/header/tables/add_default_table_in_header_spec.rb
+++ spec/docx/header/tables/add_default_table_in_header_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 describe 'Add deafult table in header' do
-  it 'Default table in footer ' do
+  it 'Default table in footer' do
     docx = builder.build_and_parse('asserts/js/docx/header/tables/add_table_in_header.js')
     expect(docx.notes.first.elements[1].rows.length).to eq(3)
   end
