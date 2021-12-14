cd829e2b33af866bfd61f54577d0af828552df30
Pavel Lobashov <shockwavenn@gmail.com>
2021-12-14 14:25:11 +0300

    Extract more

---
+++ spec/xlsx/smoke/api_range/api_range_color_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'ApiRange Color' do
+  it 'ApiRange | SetFontColor method' do
+    xlsx = builder.build_and_parse('asserts/js/xlsx/smoke/api_range/set_font_color.js')
+    expect(xlsx.worksheets.first.rows[1]
+               .cells.first.style
+               .font.color.rgb).to eq(OoxmlParser::Color.new(49, 133, 154))
+    expect(xlsx.worksheets.first.rows[3]
+               .cells.first.style
+               .font.color.theme).to eq(1)
+  end
+
+  it 'ApiRange | FontColor method' do
+    skip if builder.semver < Semantic::Version.new('5.1.0')
+    xlsx = builder.build_and_parse('asserts/js/xlsx/smoke/api_range/setter_font_color.js')
+    expect(xlsx.worksheets.first.rows[1]
+               .cells.first.style
+               .font.color.rgb).to eq(OoxmlParser::Color.new(49, 133, 154))
+    expect(xlsx.worksheets.first.rows[3]
+               .cells.first.style
+               .font.color.theme).to eq(1)
+  end
+
+  it 'ApiRange | SetFillColor method' do
+    xlsx = builder.build_and_parse('asserts/js/xlsx/smoke/api_range/set_fill_color.js')
+    expect(xlsx.worksheets.first.rows[1].cells[0].style.fill_color.pattern_fill.background_color).to eq(OoxmlParser::Color.new(255, 224, 204))
+    expect(xlsx.worksheets.first.rows[1].cells[0].style.fill_color.pattern_fill.foreground_color).to eq(OoxmlParser::Color.new(255, 224, 204))
+    expect(xlsx.worksheets.first.rows[1].cells[0].raw_text).to eq('This is the cell with a color set to its background')
+    expect(xlsx.worksheets.first.rows[3].cells[0].style.fill_color).to be_nil
+    expect(xlsx.worksheets.first.rows[3].cells[0].raw_text).to eq('This is the cell with a default background color')
+  end
+
+  it 'ApiRange | FillColor method' do
+    skip if builder.semver < Semantic::Version.new('5.1.0')
+    xlsx = builder.build_and_parse('asserts/js/xlsx/smoke/api_range/setter_fill_color.js')
+    expect(xlsx.worksheets.first.rows[1].cells[0].style.fill_color.pattern_fill.background_color).to eq(OoxmlParser::Color.new(255, 224, 204))
+    expect(xlsx.worksheets.first.rows[1].cells[0].style.fill_color.pattern_fill.foreground_color).to eq(OoxmlParser::Color.new(255, 224, 204))
+    expect(xlsx.worksheets.first.rows[1].cells[0].raw_text).to eq('This is the cell with a color set to its background')
+    expect(xlsx.worksheets.first.rows[3].cells[0].style.fill_color).to be_nil
+    expect(xlsx.worksheets.first.rows[3].cells[0].raw_text).to eq('This is the cell with a default background color')
+  end
+end
