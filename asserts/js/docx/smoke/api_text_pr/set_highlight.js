32ef8bd846330b8a3ada29f4ae4cf12a7fff4e7c
Pavel Lobashov <ShockwaveNN@gmail.com>
2022-05-16 14:06:41 +0300

    Actualize changes for DocumentBuilder v7.1.0 (#668)
    * Actualize changes for DocumentBuilder v7.1.0
    
    1. Highlight method changed accroding to bug 55266
    2. Pending test with 56263
    
    * Fix incorrect script data
    
    According to comment in
    https://bugzilla.onlyoffice.com/show_bug.cgi?id=56337
    
    * Fix tests accroding to:
    
    https://bugzilla.onlyoffice.com/show_bug.cgi?id=56337
    
    * Remove non-actual pending

--- asserts/js/docx/smoke/api_text_pr/set_highlight.js
+++ asserts/js/docx/smoke/api_text_pr/set_highlight.js
@@ -2,7 +2,7 @@ builder.CreateFile("docx");
 var oDocument = Api.GetDocument();
 var oParagraph, oTextPr;
 oTextPr = oDocument.GetDefaultTextPr();
-oTextPr.SetHighlight(255, 255, 0, false);
+oTextPr.SetHighlight("yellow", false);
 oParagraph = oDocument.GetElement(0);
 oParagraph.AddText("A sample text highlighted with yellow color using the text properties.");
 builder.SaveFile("docx", "SetHighlight.docx");
