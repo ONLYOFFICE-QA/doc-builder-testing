7f32b0ce7647ac5433959f6578cf76b84afee8b1
MaxSorokin <askonev007@gmail.com>
2022-09-06 17:24:06 +0300

    Refactoring add_endnote_cross_fer script

--- asserts/js/docx/paragraph/cross_reference/add_endnote_cross_ref.js
+++ asserts/js/docx/paragraph/cross_reference/add_endnote_cross_ref.js
@@ -1,24 +1,24 @@
 builder.CreateFile("docx");
 let oDocument = Api.GetDocument();
 let oParagraph = oDocument.GetElement(0);
-oParagraph.AddText('Endnote Reference');
+    oParagraph.AddText('Endnote Reference');
 let oRange = oParagraph.GetRange(7, 16);
-oRange.Select();
-oDocument.AddEndnote();
+    oRange.Select();
+    oDocument.AddEndnote();
 let aEnd = oDocument.GetEndNotesFirstParagraphs();
-aEnd[0].AddText('This is a creating footnote');
+    aEnd[0].AddText('This is a creating footnote');
 
 let oParagraph1 = Api.CreateParagraph();
-oParagraph1.AddText('Endnote number ');
+    oParagraph1.AddText('Endnote number ');
 let oParagraph2 = Api.CreateParagraph();
-oParagraph2.AddText('Page Number ');
+    oParagraph2.AddText('Page Number ');
 let oParagraph3 = Api.CreateParagraph();
-oParagraph3.AddText('Above/Below ');
+    oParagraph3.AddText('Above/Below ');
 let oParagraph4 = Api.CreateParagraph();
-oParagraph4.AddText('Form Endnote number');
+    oParagraph4.AddText('Form Endnote number');
 
 let arrPar = [oParagraph1, oParagraph2, oParagraph3, oParagraph4];
-arrPar.forEach(element => oDocument.Push(element));
+    arrPar.forEach(element => oDocument.Push(element));
 
 oParagraph1.AddEndnoteCrossRef("endnoteNum",aEnd[0], true, false);
 oParagraph2.AddEndnoteCrossRef("pageNum", aEnd[0], true, false);
