1e27bb90eb0b26da8fee320118b38b8f12ac414a
Sorokin Maksim <60688343+askonev@users.noreply.github.com>
2022-10-04 19:18:35 +0300

    Add test for bug 56159 (#732)

---
+++ asserts/js/xlsx/specific/bug_56159.js
@@ -0,0 +1,5 @@
+builder.CreateFile("xlsx");
+var oWorksheet = Api.GetActiveSheet();
+oWorksheet.GetCells(1, 1).SetValue('first cell');
+builder.SaveFile("xlsx", "First_cell.xlsx");
+builder.CloseFile();
---
+++ spec/xlsx/regression/api_worksheet_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+describe 'Worksheet Api regression' do
+  it 'GetCells | bug 56159' do
+    xlsx = builder.build_and_parse('asserts/js/xlsx/specific/bug_56159.js')
+    expect(xlsx.worksheets.first.rows.first.cells.first.raw_text).to eq('first cell')
+  end
+end
