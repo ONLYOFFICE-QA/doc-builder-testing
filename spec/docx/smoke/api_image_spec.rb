93cc4caf07c0661f14a30c05e53ec6a1764ee6b1
Sorokin Maksim <60688343+askonev@users.noreply.github.com>
2022-10-04 19:55:18 +0300

    Added check getters for size image (#746)

--- spec/docx/smoke/api_image_spec.rb
+++ spec/docx/smoke/api_image_spec.rb
@@ -6,4 +6,10 @@ describe 'ApiImage section tests' do
     docx = builder.build_and_parse('asserts/js/docx/smoke/api_image/get_class_type.js')
     expect(docx.elements[1].nonempty_runs.first.text).to eq('Class Type = image')
   end
+
+  it 'ApiImage | GetWeight/GetHeight methods' do
+    docx = builder.build_and_parse('asserts/js/docx/smoke/api_image/get_size_image.js')
+    expect(docx.elements[0].nonempty_runs.first.text).to eq(OoxmlParser::OoxmlSize.new(2_188_800, :emu).value.to_s)
+    expect(docx.elements[1].nonempty_runs.first.text).to eq(OoxmlParser::OoxmlSize.new(1_386_000, :emu).value.to_s)
+  end
 end
