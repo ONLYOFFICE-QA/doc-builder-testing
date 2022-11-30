2ee9c9af2731e83a9fece8f3b846f89855c50524
MaxSorokin <askonev007@gmail.com>
2022-11-30 07:51:35 +0400

    Reloading of negative conversion tests

--- spec/docx/conversion/negative_docx_conversion_spec.rb
+++ spec/docx/conversion/negative_docx_conversion_spec.rb
@@ -3,22 +3,22 @@
 require 'spec_helper'
 describe 'All formats negative tests from Docx' do
   it 'All formats | From Docx to Xlsx' do
-    expect { builder.build_and_parse('asserts/js/conversion/negative/docx_to_xlsx.js') }.to raise_error(StandardError)
+    expect { builder.build_and_parse('asserts/js/conversion/negative/docx/docx_to_xlsx.js') }.to raise_error(StandardError)
   end
 
   it 'All formats | From Docx to Ods' do
-    expect { builder.build_and_parse('asserts/js/conversion/negative/docx_to_ods.js') }.to raise_error(StandardError)
+    expect { builder.build_and_parse('asserts/js/conversion/negative/docx/docx_to_ods.js') }.to raise_error(StandardError)
   end
 
   it 'All formats | From Docx to Csv' do
-    expect { builder.build_and_parse('asserts/js/conversion/negative/docx_to_csv.js') }.to raise_error(StandardError)
+    expect { builder.build_and_parse('asserts/js/conversion/negative/docx/docx_to_csv.js') }.to raise_error(StandardError)
   end
 
   it 'All formats | From Docx to Pptx' do
-    expect { builder.build_and_parse('asserts/js/conversion/negative/docx_to_pptx.js') }.to raise_error(StandardError)
+    expect { builder.build_and_parse('asserts/js/conversion/negative/docx/docx_to_pptx.js') }.to raise_error(StandardError)
   end
 
   it 'All formats | From Docx to Odp' do
-    expect { builder.build_and_parse('asserts/js/conversion/negative/docx_to_odp.js') }.to raise_error(StandardError)
+    expect { builder.build_and_parse('asserts/js/conversion/negative/docx/docx_to_odp.js') }.to raise_error(StandardError)
   end
 end
--- spec/pptx/conversion/negative_pptx_conversion_spec.rb
+++ spec/pptx/conversion/negative_pptx_conversion_spec.rb
@@ -3,30 +3,30 @@
 require 'spec_helper'
 describe 'All formats tests from Pptx negative' do
   it 'All formats | From Pptx to Docx' do
-    expect { builder.build_and_parse('asserts/js/conversion/negative/pptx_to_docx.js') }.to raise_error(StandardError)
+    expect { builder.build_and_parse('asserts/js/conversion/negative/pptx/pptx_to_docx.js') }.to raise_error(StandardError)
   end
 
   it 'All formats | From Pptx to Odt' do
-    expect { builder.build_and_parse('asserts/js/conversion/negative/pptx_to_odt.js') }.to raise_error(StandardError)
+    expect { builder.build_and_parse('asserts/js/conversion/negative/pptx/pptx_to_odt.js') }.to raise_error(StandardError)
   end
 
   it 'All formats | From Pptx to Txt' do
-    expect { builder.build_and_parse('asserts/js/conversion/negative/pptx_to_rtf.js') }.to raise_error(StandardError)
+    expect { builder.build_and_parse('asserts/js/conversion/negative/pptx/pptx_to_txt.js') }.to raise_error(StandardError)
   end
 
   it 'All formats | From Pptx to Rtf' do
-    expect { builder.build_and_parse('asserts/js/conversion/negative/pptx_to_rtf.js') }.to raise_error(StandardError)
+    expect { builder.build_and_parse('asserts/js/conversion/negative/pptx/pptx_to_rtf.js') }.to raise_error(StandardError)
   end
 
   it 'All formats | From Pptx to Xlsx' do
-    expect { builder.build_and_parse('asserts/js/conversion/negative/pptx_to_xlsx.js') }.to raise_error(StandardError)
+    expect { builder.build_and_parse('asserts/js/conversion/negative/pptx/pptx_to_xlsx.js') }.to raise_error(StandardError)
   end
 
   it 'All formats | From Pptx to Ods' do
-    expect { builder.build_and_parse('asserts/js/conversion/negative/pptx_to_ods.js') }.to raise_error(StandardError)
+    expect { builder.build_and_parse('asserts/js/conversion/negative/pptx/pptx_to_ods.js') }.to raise_error(StandardError)
   end
 
   it 'All formats | From Pptx to Csv' do
-    expect { builder.build_and_parse('asserts/js/conversion/negative/pptx_to_csv.js') }.to raise_error(StandardError)
+    expect { builder.build_and_parse('asserts/js/conversion/negative/pptx/pptx_to_csv.js') }.to raise_error(StandardError)
   end
 end
