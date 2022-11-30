776ebe4d078a9db9873fb6e2157d7e7c6251e579
MaxSorokin <askonev007@gmail.com>
2022-11-30 07:27:27 +0400

    Reloading of positive conversion tests

--- spec/pptx/conversion/pptx_convesion_spec.rb
+++ spec/pptx/conversion/pptx_convesion_spec.rb
@@ -3,17 +3,17 @@
 require 'spec_helper'
 describe 'All formats tests from Pptx' do
   it 'All formats | From Pptx to Pptx', critical: true do
-    pptx = builder.build_and_parse('asserts/js/conversion/pptx_to_pptx.js')
+    pptx = builder.build_and_parse('asserts/js/conversion/pptx/pptx_to_pptx.js')
     expect(pptx).to be_with_data
   end
 
   it 'All formats | From Pptx to Odp' do
-    output_file = builder.build_file('asserts/js/conversion/pptx_to_odp.js')
+    output_file = builder.build_file('asserts/js/conversion/pptx/pptx_to_odp.js')
     expect(builder).not_to be_file_empty(output_file)
   end
 
   it 'All formats | From Pptx to Pdf', critical: true do
-    output_file = builder.build_file('asserts/js/conversion/pptx_to_pdf.js')
+    output_file = builder.build_file('asserts/js/conversion/pptx/pptx_to_pdf.js')
     expect(builder).not_to be_file_empty(output_file)
   end
 end
--- spec/xlsx/conversion/xlsx_conversion_spec.rb
+++ spec/xlsx/conversion/xlsx_conversion_spec.rb
@@ -3,22 +3,22 @@
 require 'spec_helper'
 describe 'From Xlsx', critical: true do
   it 'All formats | From Xlsx to Xlsx' do
-    xlsx = builder.build_and_parse('asserts/js/conversion/xlsx_to_xlsx.js')
+    xlsx = builder.build_and_parse('asserts/js/conversion/xlsx/xlsx_to_xlsx.js')
     expect(xlsx).to be_with_data
   end
 
   it 'All formats | From Xlsx to Ods' do
-    output_file = builder.build_file('asserts/js/conversion/xlsx_to_ods.js')
+    output_file = builder.build_file('asserts/js/conversion/xlsx/xlsx_to_ods.js')
     expect(builder).not_to be_file_empty(output_file)
   end
 
   it 'All formats | From Xlsx to Pdf' do
-    output_file = builder.build_file('asserts/js/conversion/xlsx_to_pdf.js')
+    output_file = builder.build_file('asserts/js/conversion/xlsx/xlsx_to_pdf.js')
     expect(builder).not_to be_file_empty(output_file)
   end
 
   it 'All formats | From Xlsx to Csv' do
-    output_file = builder.build_file('asserts/js/conversion/xlsx_to_csv.js')
+    output_file = builder.build_file('asserts/js/conversion/xlsx/xlsx_to_csv.js')
     expect(builder).not_to be_file_empty(output_file)
   end
 end
