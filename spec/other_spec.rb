bbb7dbae67f15db3217d3b18d81a2707d760a427
MaxSorokin <askonev007@gmail.com>
2022-11-30 06:52:14 +0400

    Move js script away from spec

--- spec/other_spec.rb
+++ spec/other_spec.rb
@@ -4,9 +4,9 @@ require 'spec_helper'
 describe 'Other results' do
   it 'Correct error for builder.CloseFile();' do
     if web_builder?
-      expect { builder.build_and_parse('spec/other/close_file.js') }.to raise_error(EmptyUrlsInWebBuilderResponse)
+      expect { builder.build_and_parse('asserts/other/close_file.js') }.to raise_error(EmptyUrlsInWebBuilderResponse)
     else
-      expect { builder.build_and_parse('spec/other/close_file.js') }.to raise_error(DocBuilderFileCreationError)
+      expect { builder.build_and_parse('asserts/other/close_file.js') }.to raise_error(DocBuilderFileCreationError)
     end
   end
 end
