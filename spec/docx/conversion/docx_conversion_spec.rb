1c2158bd05d65d15db2ff266ff294a9a178f76c9
MaxSorokin <askonev007@gmail.com>
2022-12-16 17:55:58 +0400

    Tests for resaving docx to different document formats

--- spec/docx/conversion/docx_conversion_spec.rb
+++ spec/docx/conversion/docx_conversion_spec.rb
@@ -2,13 +2,78 @@
 
 require 'spec_helper'
 describe 'Conversion tests from Docx' do
-  it 'Conversion | Docx to Docx', :critical do
-    docx = builder.build_and_parse('js/conversion/docx/docx_to_docx.js')
-    expect(docx).to be_with_data
+  it 'Conversion | docx to docx', critical: true do
+    parsed_output_file = builder.build_and_parse('js/conversion/docx/docx_to_docx.js')
+    expect(parsed_output_file).to be_with_data
   end
 
-  it 'Conversion | Docx to Odt' do
+  it 'Conversion | docx to Docxf' do
+    parsed_output_file = builder.build_and_parse('js/conversion/docx/docx_to_docxf.js')
+    ext = builder.recognize_output_format(File.read('js/conversion/docx/docx_to_docxf.js'))
+    expect(File.extname(parsed_output_file.file_path).strip.downcase[1..]).to eq(ext)
+    expect(parsed_output_file).to be_with_data
+  end
+
+  it 'Conversion | docx to docm' do
+    parsed_output_file = builder.build_and_parse('js/conversion/docx/docx_to_docm.js')
+    ext = builder.recognize_output_format(File.read('js/conversion/docx/docx_to_docm.js'))
+    expect(File.extname(parsed_output_file.file_path).strip.downcase[1..]).to eq(ext)
+    expect(parsed_output_file).to be_with_data
+  end
+
+  it 'Conversion | docx to dotx' do
+    parsed_output_file = builder.build_and_parse('js/conversion/docx/docx_to_dotx.js')
+    ext = builder.recognize_output_format(File.read('js/conversion/docx/docx_to_dotx.js'))
+    expect(File.extname(parsed_output_file.file_path).strip.downcase[1..]).to eq(ext)
+    expect(parsed_output_file).to be_with_data
+  end
+
+  it 'Conversion | docx to dotm' do
+    parsed_output_file = builder.build_and_parse('js/conversion/docx/docx_to_dotm.js')
+    ext = builder.recognize_output_format(File.read('js/conversion/docx/docx_to_dotm.js'))
+    expect(File.extname(parsed_output_file.file_path).strip.downcase[1..]).to eq(ext)
+    expect(parsed_output_file).to be_with_data
+  end
+
+  it 'Conversion | docx to pdf', critical: true do
+    parsed_output_file = builder.build_and_parse('js/conversion/docx/docx_to_pdf.js')
+    ext = builder.recognize_output_format(File.read('js/conversion/docx/docx_to_pdf.js'))
+    expect(File.extname(parsed_output_file.file_path).strip.downcase[1..]).to eq(ext)
+    expect(parsed_output_file.pages[0][:text]).to eq('This is just a sample text. Nothing special.')
+  end
+
+  it 'Conversion | docx to epub', bug: true do
+    output_file = builder.build_file('js/conversion/docx/docx_to_epub.js')
+    ext = builder.recognize_output_format(File.read('js/conversion/docx/docx_to_epub.js'))
+    expect(File.extname(output_file).strip.downcase[1..]).to eq(ext)
+    expect(builder).not_to be_file_empty(output_file)
+  end
+
+  it 'Conversion | docx to fb2', bug: true do
+    output_file = builder.build_file('js/conversion/docx/docx_to_fb2.js')
+    ext = builder.recognize_output_format(File.read('js/conversion/docx/docx_to_fb2.js'))
+    expect(File.extname(output_file).strip.downcase[1..]).to eq(ext)
+    expect(builder).not_to be_file_empty(output_file)
+  end
+
+  it 'Conversion | docx to html', bug: true do
+    output_file = builder.build_file('js/conversion/docx/docx_to_html.js')
+    ext = builder.recognize_output_format(File.read('js/conversion/docx/docx_to_html.js'))
+    expect(File.extname(output_file).strip.downcase[1..]).to eq(ext)
+    expect(builder).not_to be_file_empty(output_file)
+  end
+
+  it 'Conversion | docx to ott', bug: true do
+    output_file = builder.build_file('js/conversion/docx/docx_to_ott.js')
+    ext = builder.recognize_output_format(File.read('js/conversion/docx/docx_to_ott.js'))
+    expect(File.extname(output_file).strip.downcase[1..]).to eq(ext)
+    expect(builder).not_to be_file_empty(output_file)
+  end
+
+  it 'Conversion | docx to odt' do
     output_file = builder.build_file('js/conversion/docx/docx_to_odt.js')
+    ext = builder.recognize_output_format(File.read('js/conversion/docx/docx_to_odt.js'))
+    expect(File.extname(output_file).strip.downcase[1..]).to eq(ext)
     expect(builder).not_to be_file_empty(output_file)
   end
 
@@ -17,13 +82,24 @@ describe 'Conversion tests from Docx' do
     expect(builder).not_to be_file_empty(output_file)
   end
 
-  it 'Conversion | Docx to Rtf', :critical do
-    output_file = builder.build_file('js/conversion/docx/docx_to_rtf.js')
+  it 'Conversion | docx to pdfa', bug: true do
+    output_file = builder.build_file('js/conversion/docx/docx_to_pdfa.js')
+    ext = builder.recognize_output_format(File.read('js/conversion/docx/docx_to_pdfa.js'))
+    expect(File.extname(output_file).strip.downcase[1..]).to eq(ext)
     expect(builder).not_to be_file_empty(output_file)
   end
 
-  it 'Conversion | Docx to Txt', :critical do
+  it 'Conversion | docx to rtf', critical: true do
+    output_file = builder.build_file('js/conversion/docx/docx_to_rtf.js')
+    ext = builder.recognize_output_format(File.read('js/conversion/docx/docx_to_rtf.js'))
+    expect(File.extname(output_file).strip.downcase[1..]).to eq(ext)
+    expect(builder).not_to be_file_empty(output_file)
+  end
+
+  it 'Conversion | Docx to Txt', critical: true do
     output_file = builder.build_file('js/conversion/docx/docx_to_txt.js')
+    ext = builder.recognize_output_format(File.read('js/conversion/docx/docx_to_txt.js'))
+    expect(File.extname(output_file).strip.downcase[1..]).to eq(ext)
     expect(builder).not_to be_file_empty(output_file)
   end
 end
