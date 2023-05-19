1d020f22fd8b21c39969fc60e3a8458a4154ad93
MaxSorokin <askonev007@gmail.com>
2023-05-19 14:11:44 +0300

    GetNumberFormat #`59370`

---
+++ spec/test_data.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module TestData
+  def self.number_formats
+    ['General', '0.00', '$#,##0.00', '_($* #,##0.00_)', 'm/d/yyyy',
+     '[$-F800]dddd, mmmm dd, yyyy', '[$-F400]h:mm:ss AM/PM',
+     '0.00%', '0%', '# ?/?', '0.00E+00'].freeze
+  end
+end
