7f46c611bd7cbc53b07a1f7f1493e3c24c965647
Sorokin Maksim <60688343+askonev@users.noreply.github.com>
2023-07-03 16:58:56 +0300

    Update to 7.4.0 dockerfiles (next-release: 7.4.1) & fixed: bug `59740` (#915)
    * fixed: fixed bug 59740
    
    * update to 7.4.1

--- js/xlsx/smoke/api_range/getter_col.js
+++ js/xlsx/smoke/api_range/getter_col.js
@@ -1,6 +1,6 @@
 builder.CreateFile("xlsx");
-var oWorksheet = Api.GetActiveSheet();
-var oRange = oWorksheet.GetRange("D9").Col;
-oWorksheet.GetRange("A2").SetValue(oRange.toString());
+    var oWorksheet = Api.GetActiveSheet();
+    var oRange = oWorksheet.GetRange("D9").Col;
+        oWorksheet.GetRange("A2").SetValue(oRange.toString());
 builder.SaveFile("xlsx", "GetCol.xlsx");
 builder.CloseFile();
