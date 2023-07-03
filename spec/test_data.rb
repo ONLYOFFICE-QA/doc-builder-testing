4ccc791c33c0e341a1d048eb84410d4e64c0ed4a
MaxSorokin <askonev007@gmail.com>
2023-07-03 17:18:37 +0300

    Add documenttation via TestData

--- spec/test_data.rb
+++ spec/test_data.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
+# Test data for autotest
 module TestData
+  # CDE number formats (7.4.0)
   def self.number_formats
     ['General', '0.00', '$#,##0.00', '_($* #,##0.00_)', 'm/d/yyyy',
      '[$-F800]dddd, mmmm dd, yyyy', '[$-F400]h:mm:ss AM/PM',
