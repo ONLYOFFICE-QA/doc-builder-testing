2e8a8f7eaa9658a2b612a64113ac945b557bc836
Yarosalv Maslov <148754748+Yar04ek@users.noreply.github.com>
2023-11-22 11:42:03 +0100

    add JSON test to table row pr

--- spec/docx/smoke/api_table_row_properties_spec.rb
+++ spec/docx/smoke/api_table_row_properties_spec.rb
@@ -1,33 +1,28 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-describe('ApiTableRowPr section tests') do
+describe 'ApiTableRowPr section tests' do
   it 'ApiTableRowPr | GetClassType method' do
-    docx = builder.build_and_parse('js/docx/smoke/api_table_row_pr/set_height.js')
-    expected_height = OoxmlParser::OoxmlSize.new(720, :twip)
-    actual_height = docx.elements[1].properties.table_style.table_row_properties.height.value
-    expect(actual_height).to eq(expected_height)
+    docx = builder.build_and_parse('js/docx/smoke/api_table_row_pr/get_class_type.js')
+    expect(docx.elements.first.nonempty_runs.first.text).to eq('Class Type = tableRowPr')
   end
 
   it 'ApiTableRowPr | SetHeight method' do
     docx = builder.build_and_parse('js/docx/smoke/api_table_row_pr/set_height.js')
-    expect(docx.elements[0].nonempty_runs[0].text).to eq('We create a 3x3 table and set the height of half an inch to all the rows:')
-    table_row_pr = docx.elements[1].properties.table_style.table_row_properties
-    expect(table_row_pr.height.value).to eq(OoxmlParser::OoxmlSize.new(720, :twip))
+    expect(docx.elements[1].properties.table_style.table_row_properties.height.value).to eq(OoxmlParser::OoxmlSize.new(720, :twip))
   end
 
   it 'ApiTableRowPr | SetTableHeader method' do
-    # Build and parse the document
     docx = builder.build_and_parse('js/docx/smoke/api_table_row_pr/set_table_header.js')
-    # Check the content of the first paragraph
-    expected_text = 'We create a 3x3 table and set all table rows as the table headers:'
-    actual_text = docx.elements.first.nonempty_runs.first.text
-    expect(actual_text).to eq(expected_text)
-    # Check if the table has the correct style
-    table_style = docx.elements[1].properties.table_style
-    expect(table_style).not_to be_nil
-    expect(table_style.table_row_properties).not_to be_nil
-    # Check if the table header property is set to true
-    expect(table_style.table_row_properties.table_header).to be(true)
+    expect(docx.elements[1].properties.table_style.table_row_properties.table_header).to be_truthy
+  end
+
+  it 'ApiTableRowPr | ToJSON method' do
+    docx = builder.build_and_parse('js/docx/smoke/api_table_row_pr/to_json.js')
+    json_text = docx.elements[0].nonempty_runs[0].text
+    parsed_json = JSON.parse(json_text)
+    expect(parsed_json['type']).to eq('Table')
+    expect(parsed_json['styles']['132']['name']).to eq('List Table 5 Dark')
+    expect(docx.elements[1].properties.table_style.name).to eq('List Table 5 Dark')
   end
 end
