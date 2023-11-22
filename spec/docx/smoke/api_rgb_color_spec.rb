b91b2a462507dda5c81161204a7f7e05e07ab37a
Yarosalv Maslov <148754748+Yar04ek@users.noreply.github.com>
2023-11-22 10:36:21 +0100

    add JSON test for RGB color

--- spec/docx/smoke/api_rgb_color_spec.rb
+++ spec/docx/smoke/api_rgb_color_spec.rb
@@ -1,28 +1,19 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-
 describe 'ApiRGBColor section tests' do
   it 'ApiRGBColor | GetClassType method' do
     docx = builder.build_and_parse('js/docx/smoke/api_rgb_color/get_class_type.js')
-    docx.elements[1].nonempty_runs[0].text.downcase.delete(' ').delete('=')
-    class_type_prefix = 'class type = '
-    actual_class_type = docx.elements[1].nonempty_runs[0].text.downcase
-    actual_class_type.slice!(class_type_prefix) if actual_class_type.start_with?(class_type_prefix)
-    expected_class_type = 'rgbcolor'
-    expect(actual_class_type).to eq(expected_class_type)
-    json_text = docx.elements[0].nonempty_runs.last.text.strip
-    if json_text.start_with?('{')
-      begin
-        json = JSON.parse(json_text)
-        color_from_json = json['graphic']['spPr']['fill']['fill']['gsLst'][1]['color']['color']['rgba']
-        expected_color = [255, 164, 101]
-        expect(color_from_json).to eq(expected_color)
-      rescue JSON::ParserError => e
-        puts "Error parsing JSON: #{e.message}"
-      end
-    else
-      puts 'Invalid JSON format'
-    end
+    expect(docx.elements[1].nonempty_runs.first.text).to eq('Class Type = rgbColor')
+
+  end
+
+  it 'ApiRGBColor | ToJSON method' do
+    docx = builder.build_and_parse('js/docx/smoke/api_rgb_color/to_json.js')
+    expect(docx.elements[1].nonempty_runs.first.text).to eq('Class Type = rgbColor')
+    json = JSON.parse(docx.elements[0].nonempty_runs.first.text)
+    color_from_json = json['graphic']['spPr']['fill']['fill']['gsLst'][1]['color']['color']['rgba']
+    expected_color = [255, 164, 101] # Укажите ожидаемый цвет в формате RGBA
+    expect(color_from_json).to eq(expected_color)
   end
 end
