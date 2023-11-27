31a9f5a65ca3cd605de6a9d8b43f7ea9f860df62
Yar04ek <yaroslavmaslov0951@gmail.com>
2023-11-27 13:42:59 +0100

    Added: creation of GlobalVariable document and rewritten test

--- js/docx/smoke/api_rgb_color/to_json.js
+++ js/docx/smoke/api_rgb_color/to_json.js
@@ -2,9 +2,8 @@ builder.CreateFile("docx");
 let oDocument = Api.GetDocument();
 let oParagraph = oDocument.GetElement(0);
 let oRGBColor = Api.CreateRGBColor(255, 111, 61);
-let json = oRGBColor.ToJSON();
-oParagraph.AddText(json);
-let oRGBColorFromJSON = Api.FromJSON(json);
+let jsonRGBColor = oRGBColor.ToJSON();
+oParagraph.AddText(jsonRGBColor);
 let oGs1 = Api.CreateGradientStop(oRGBColor, 0);
 let oGs2 = Api.CreateGradientStop(Api.CreateRGBColor(255, 164, 101), 100000);
 let oFill = Api.CreateLinearGradientFill([oGs1, oGs2], 5400000);
@@ -12,9 +11,32 @@ let oStroke = Api.CreateStroke(0, Api.CreateNoFill());
 let oDrawing = Api.CreateShape("rect", 5930900, 395605, oFill, oStroke);
 let bWriteNumberings = false;
 let bWriteStyles = true;
-let sDrawingJson = oDrawing.ToJSON(bWriteNumberings, bWriteStyles);
-let oParagraph1 = Api.CreateParagraph();
-oParagraph1.AddText(sDrawingJson);
-oDocument.Push(oParagraph1);
+let jsonDrawing = oDrawing.ToJSON(bWriteNumberings, bWriteStyles);
+GlobalVariable["JSON_RGBColor"] = jsonRGBColor;
+GlobalVariable["JSON_Drawing"] = jsonDrawing;
+builder.CloseFile();
+
+
+/////////////////////
+
+
+builder.CreateFile("docx");
+let oNewDocument = Api.GetDocument();
+let jsonRGBColor = GlobalVariable["JSON_RGBColor"];
+let jsonDrawing = GlobalVariable["JSON_Drawing"];
+let oParagraphDrawing = Api.CreateParagraph();
+oParagraphDrawing.AddText(jsonDrawing);
+oNewDocument.Push(oParagraphDrawing);
+let oRGBColorFromJSON = Api.FromJSON(jsonRGBColor);
+let oParagraphRGBColorObject = Api.CreateParagraph();
+oParagraphRGBColorObject.AddDrawing(oRGBColorFromJSON);
+oNewDocument.Push(oParagraphRGBColorObject);
+let oParagraphRGBColor = Api.CreateParagraph();
+oParagraphRGBColor.AddText(jsonRGBColor);
+oNewDocument.Push(oParagraphRGBColor);
+let oDrawingFromJSON = Api.FromJSON(jsonDrawing);
+let oParagraphDrawingObject = Api.CreateParagraph();
+oParagraphDrawingObject.AddDrawing(oDrawingFromJSON);
+oNewDocument.Push(oParagraphDrawingObject);
 builder.SaveFile("docx", "RGBColorToJSON.docx");
 builder.CloseFile();
