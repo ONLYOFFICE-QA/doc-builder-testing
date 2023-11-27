3d2553b1c13847c6d109ccfb87f266ab42ace443
Yar04ek <yaroslavmaslov0951@gmail.com>
2023-11-27 13:42:59 +0100

    creation of GlobalVariable document and rewritten test

--- spec/docx/smoke/api_rgb_color_spec.rb
+++ spec/docx/smoke/api_rgb_color_spec.rb
@@ -9,8 +9,8 @@ describe 'ApiRGBColor section tests' do
 
   it 'ApiRGBColor | ToJSON method' do
     docx = builder.build_and_parse('js/docx/smoke/api_rgb_color/to_json.js')
-    first_paragraph_text = docx.elements[0].nonempty_runs.first.text
-    third_paragraph_text = docx.elements[1].nonempty_runs.first.text
-    expect(third_paragraph_text).to include(first_paragraph_text)
+    first_paragraph_text = '{"rgba":{"red":0,"green":0,"blue":0,"alpha":255},"color":{"rgba":{"red":255,"green":111,"blue":61,"alpha":255},"type":"srgb"},"type":"uniColor"}'
+    second_paragraph_text = docx.elements[1].nonempty_runs.first.text
+    expect(second_paragraph_text).to include(first_paragraph_text)
   end
 end
