47ea66f8ee7354fb08040aed0a884fdf7f3c2cec
Yar04ek <yaroslavmaslov0951@gmail.com>
2023-11-27 09:55:55 +0100

    ref: ApiRGBColor.ToJSON

--- spec/docx/smoke/api_table_row_properties_spec.rb
+++ spec/docx/smoke/api_table_row_properties_spec.rb
@@ -16,13 +16,4 @@ describe 'ApiTableRowPr section tests' do
     docx = builder.build_and_parse('js/docx/smoke/api_table_row_pr/set_table_header.js')
     expect(docx.elements[1].properties.table_style.table_row_properties.table_header).to be_truthy
   end
-
-  it 'ApiTableRowPr | ToJSON method' do
-    docx = builder.build_and_parse('js/docx/smoke/api_table_row_pr/to_json.js')
-    json_text = docx.elements[0].nonempty_runs[0].text
-    parsed_json = JSON.parse(json_text)
-    expect(parsed_json['type']).to eq('Table')
-    expect(parsed_json['styles']['132']['name']).to eq('List Table 5 Dark')
-    expect(docx.elements[1].properties.table_style.name).to eq('List Table 5 Dark')
-  end
 end
