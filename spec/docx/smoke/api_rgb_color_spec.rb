4950d70d3374c5435e7d5265952e84f5388f4f17
Yar04ek <yaroslavmaslov0951@gmail.com>
2023-11-27 09:55:55 +0100

    ref: ApiRGBColor.ToJSON

--- spec/docx/smoke/api_rgb_color_spec.rb
+++ spec/docx/smoke/api_rgb_color_spec.rb
@@ -5,17 +5,12 @@ describe 'ApiRGBColor section tests' do
   it 'ApiRGBColor | GetClassType method' do
     docx = builder.build_and_parse('js/docx/smoke/api_rgb_color/get_class_type.js')
     expect(docx.elements[1].nonempty_runs.first.text).to eq('Class Type = rgbColor')
-
   end
-
 
   it 'ApiRGBColor | ToJSON method' do
     docx = builder.build_and_parse('js/docx/smoke/api_rgb_color/to_json.js')
-    p docx
-    # expect(docx.paragraphs[0].text).to include(GlobalVariable['RGBColorJSON'])
-    # json = JSON.parse(docx.elements[1].nonempty_runs.first.text)
-    # expected_rgba = (GlobalVariable['RGBColorJSON'])['graphic']['spPr']['fill']['fill']['gsLst'][1]['color']['color']['rgba']
-    # expect(json['graphic']['spPr']['fill']['fill']['gsLst'][1]['color']['color']['rgba']).to eq(expected_rgba)
+    first_paragraph_text = docx.elements[0].nonempty_runs.first.text
+    third_paragraph_text = docx.elements[1].nonempty_runs.first.text
+    expect(third_paragraph_text).to include(first_paragraph_text)
   end
-
 end
