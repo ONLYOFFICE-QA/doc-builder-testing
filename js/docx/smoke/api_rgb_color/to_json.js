49b5fcea9117c58d549dff3f74bbf91585cc806e
Yar04ek <yaroslavmaslov0951@gmail.com>
2023-11-27 14:32:28 +0100

    fix to_json

--- js/docx/smoke/api_rgb_color/to_json.js
+++ js/docx/smoke/api_rgb_color/to_json.js
@@ -4,16 +4,7 @@ let oParagraph = oDocument.GetElement(0);
 let oRGBColor = Api.CreateRGBColor(255, 111, 61);
 let jsonRGBColor = oRGBColor.ToJSON();
 oParagraph.AddText(jsonRGBColor);
-let oGs1 = Api.CreateGradientStop(oRGBColor, 0);
-let oGs2 = Api.CreateGradientStop(Api.CreateRGBColor(255, 164, 101), 100000);
-let oFill = Api.CreateLinearGradientFill([oGs1, oGs2], 5400000);
-let oStroke = Api.CreateStroke(0, Api.CreateNoFill());
-let oDrawing = Api.CreateShape("rect", 5930900, 395605, oFill, oStroke);
-let bWriteNumberings = false;
-let bWriteStyles = true;
-let jsonDrawing = oDrawing.ToJSON(bWriteNumberings, bWriteStyles);
 GlobalVariable["JSON_RGBColor"] = jsonRGBColor;
-GlobalVariable["JSON_Drawing"] = jsonDrawing;
 builder.CloseFile();
 
 
@@ -23,20 +14,31 @@ builder.CloseFile();
 builder.CreateFile("docx");
 let oNewDocument = Api.GetDocument();
 let jsonRGBColor = GlobalVariable["JSON_RGBColor"];
-let jsonDrawing = GlobalVariable["JSON_Drawing"];
-let oParagraphDrawing = Api.CreateParagraph();
-oParagraphDrawing.AddText(jsonDrawing);
-oNewDocument.Push(oParagraphDrawing);
+let oParagraphRGBColor = oNewDocument.GetElement(0);
+oParagraphRGBColor.AddText(jsonRGBColor);
+
 let oRGBColorFromJSON = Api.FromJSON(jsonRGBColor);
 let oParagraphRGBColorObject = Api.CreateParagraph();
 oParagraphRGBColorObject.AddDrawing(oRGBColorFromJSON);
 oNewDocument.Push(oParagraphRGBColorObject);
-let oParagraphRGBColor = Api.CreateParagraph();
-oParagraphRGBColor.AddText(jsonRGBColor);
-oNewDocument.Push(oParagraphRGBColor);
-let oDrawingFromJSON = Api.FromJSON(jsonDrawing);
+
+let oGs1 = Api.CreateGradientStop(oRGBColorFromJSON, 0);
+let oGs2 = Api.CreateGradientStop(Api.CreateRGBColor(255, 164, 101), 100000);
+let oFill = Api.CreateLinearGradientFill([oGs1, oGs2], 5400000);
+let oStroke = Api.CreateStroke(0, Api.CreateNoFill());
+let oDrawingFromJSON = Api.CreateShape("rect", 5930900, 395605, oFill, oStroke);
+let bWriteNumberings = false;
+let bWriteStyles = true;
+let jsonDrawingFromJSON = oDrawingFromJSON.ToJSON(bWriteNumberings, bWriteStyles);
+
+let oParagraphDrawing = Api.CreateParagraph();
+oParagraphDrawing.AddText(jsonDrawingFromJSON);
+oNewDocument.Push(oParagraphDrawing);
+
+let oDrawingFromJSONObj = Api.FromJSON(jsonDrawingFromJSON);
 let oParagraphDrawingObject = Api.CreateParagraph();
-oParagraphDrawingObject.AddDrawing(oDrawingFromJSON);
+oParagraphDrawingObject.AddDrawing(oDrawingFromJSONObj);
 oNewDocument.Push(oParagraphDrawingObject);
+
 builder.SaveFile("docx", "RGBColorToJSON.docx");
 builder.CloseFile();
