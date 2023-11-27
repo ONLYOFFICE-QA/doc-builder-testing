e559a4abec8e661827b9e036412265797e30b19a
Yar04ek <yaroslavmaslov0951@gmail.com>
2023-11-27 08:54:30 +0100

    ref: ApiRGBColor.ToJSON

--- js/docx/smoke/api_rgb_color/to_json.js
+++ js/docx/smoke/api_rgb_color/to_json.js
@@ -1,28 +1,20 @@
 builder.CreateFile("docx");
-var oDocument = Api.GetDocument();
-var oParagraph = Api.CreateParagraph();
-var oRGBColor = Api.CreateRGBColor(255, 0, 0);
-var json = oRGBColor.ToJSON();
-oParagraph.AddText("JSON representation of RGB color: " + json);
-oDocument.Push(oParagraph);
-GlobalVariable["RGBColorJSON"] = json;
-builder.CloseFile()
-/////////////////////////
-
-builder.CreateFile("docx");
-var oDocument = Api.GetDocument();
-var oParagraph = oDocument.GetElement(0);
-var json = GlobalVariable["RGBColorJSON"];
-var oRGBColorFromJSON = Api.FromJSON(json);
-oRGBColorFromJSON.SetDefaultStyle();
-oParagraph.AddElement(oRGBColorFromJSON);
-var oParagraph1 = Api.CreateParagraph();
+let oDocument = Api.GetDocument();
+let oParagraph = oDocument.GetElement(0);
+let oRGBColor = Api.CreateRGBColor(255, 111, 61);
+let json = oRGBColor.ToJSON();
+oParagraph.AddText(json);
+let oRGBColorFromJSON = Api.FromJSON(json);
+let oGs1 = Api.CreateGradientStop(oRGBColor, 0);
+let oGs2 = Api.CreateGradientStop(Api.CreateRGBColor(255, 164, 101), 100000);
+let oFill = Api.CreateLinearGradientFill([oGs1, oGs2], 5400000);
+let oStroke = Api.CreateStroke(0, Api.CreateNoFill());
+let oDrawing = Api.CreateShape("rect", 5930900, 395605, oFill, oStroke);
+let bWriteNumberings = false;
+let bWriteStyles = true;
+let sDrawingJson = oDrawing.ToJSON(bWriteNumberings, bWriteStyles);
+let oParagraph1 = Api.CreateParagraph();
+oParagraph1.AddText(sDrawingJson);
 oDocument.Push(oParagraph1);
-oParagraph1.AddText(json);
-builder.SaveFile("docx", "RGBColorDocument.docx");
+builder.SaveFile("docx", "RGBColorToJSON.docx");
 builder.CloseFile();
-
-
-
-
-
