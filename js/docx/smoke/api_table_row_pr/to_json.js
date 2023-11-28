77b0037250b1d4c8e28260c9e76aa527ac92e438
Yar04ek <yaroslavmaslov0951@gmail.com>
2023-11-28 14:08:58 +0100

    feature: ApiTableRowPr.ToJSON

--- js/docx/smoke/api_table_row_pr/to_json.js
+++ js/docx/smoke/api_table_row_pr/to_json.js
@@ -1,38 +1,29 @@
 builder.CreateFile("docx");
-    let oDocument = Api.GetDocument();
-    let oParagraph = oDocument.GetElement(0);
-    let oTableStyle = oDocument.CreateStyle("CustomTableStyle", "table");
-        oTableStyle.SetName("My Custom Table Style");
-    let oTableRowPr = oTableStyle.GetTableRowPr();
-        oTableRowPr.SetHeight("atLeast", 720);
-    let jsonTableStyle = oTableStyle.ToJSON();
-        GlobalVariable["JSON_TableStyle"] = jsonTableStyle;
-        oParagraph.AddText(jsonTableStyle);
+let oDocument = Api.GetDocument();
+let oParagraph = oDocument.GetElement(0);
+let oTableStyle = oDocument.CreateStyle("CustomTableStyle", "table");
+oTableStyle.SetName("My Custom Table Style");
+let oTableRowPr = oTableStyle.GetTableRowPr();
+oTableRowPr.SetHeight("atLeast", 720);
+let jsonTableStyle = oTableStyle.ToJSON();
+GlobalVariable["JSON_TableStyle"] = jsonTableStyle;
+oParagraph.AddText(jsonTableStyle);
 builder.CloseFile();
 
-
-/////////////////
-
+//////////////
 
 builder.CreateFile("docx");
     let oDocument = Api.GetDocument();
+    let oParagraph = oDocument.GetElement(0);
     let jsonTableStyle = GlobalVariable["JSON_TableStyle"];
         oParagraph.AddText(jsonTableStyle);
-    let oTableStyleFromJSON = Api.FromJSON(jsonTableStyle);
-        oDocument.Push(oTableStyleFromJSON);
-    let oParagraph2 = Api.CreateParagraph();
-        oDocument.Push(oParagraph2);
     let oTableStyle = oDocument.GetStyle("My Custom Table Style");
     let oTable = Api.CreateTable(3, 3);
         oTable.SetStyle(oTableStyle);
         oDocument.Push(oTable);
     let jsonTable = oTableStyle.ToJSON();
-    let oParagraph3 = Api.CreateParagraph();
-        oDocument.Push(oParagraph3);
-        oParagraph3.AddText(jsonTable);
+    let oParagraph2 = Api.CreateParagraph();
+        oDocument.Push(oParagraph2);
         oParagraph2.AddText(jsonTable);
 builder.SaveFile("docx", "TableStyleToJSON.docx");
 builder.CloseFile();
-
-
-
