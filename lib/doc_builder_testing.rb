66d776edc8b87f9ab6a4788c16bebfc4cb4aab5b
MaxSorokin <askonev007@gmail.com>
2023-11-28 14:03:48 +0300

    fix: unify static data for specs

--- lib/doc_builder_testing.rb
+++ lib/doc_builder_testing.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
+require 'tempfile'
 require 'ooxml_parser'
 require 'onlyoffice_pdf_parser'
-require 'tempfile'
-require_relative 'doc_builder_testing/doc_builder_wrapper'
 require_relative 'doc_builder_testing/exceptions'
+require_relative 'doc_builder_testing/doc_builder_wrapper'
 require_relative 'doc_builder_testing/web_doc_builder_wrapper'
-require_relative '../spec/test_data'
-require_relative 'doc_builder_static_data'
