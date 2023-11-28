a5d3a1cadf3c3b4a558a804dab0a101b3a5b3360
MaxSorokin <askonev007@gmail.com>
2023-11-28 14:03:48 +0300

    fix: unify static data for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ Encoding.default_external = Encoding::UTF_8
 require 'bundler/setup'
 require 'rspec'
 require_relative '../lib/doc_builder_testing'
+require_relative 'doc_builder_static_data'
 
 # Get either DocBuilder of WebDocBuilder
 # depending of env var
