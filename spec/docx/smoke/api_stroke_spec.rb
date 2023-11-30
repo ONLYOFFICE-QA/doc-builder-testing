7f5b15ea5c5e907d01b967b451d4bf2de15e4507
Yar04ek <yaroslavmaslov0951@gmail.com>
2023-11-30 12:12:40 +0100

    feature: ApiStroke.ToJSON

--- spec/docx/smoke/api_stroke_spec.rb
+++ spec/docx/smoke/api_stroke_spec.rb
@@ -6,4 +6,13 @@ describe 'ApiStroke section tests' do
     docx = builder.build_and_parse('js/docx/smoke/api_stroke/get_class_type.js')
     expect(docx.elements[1].nonempty_runs.first.text).to eq('Class Type = stroke')
   end
+
+  it 'ApiStroke | ToJSON method' do
+    docx = builder.build_and_parse('js/docx/smoke/api_stroke/to_json.js')
+    first_paragraph_text = docx.elements[0].nonempty_runs.first.text
+    second_paragraph_text = docx.elements[1].nonempty_runs.first.text
+    first_paragraph_json = JSON.parse(first_paragraph_text)
+    second_paragraph_json = JSON.parse(second_paragraph_text)
+    expect(second_paragraph_json.to_s).to include(first_paragraph_json.to_s)
+  end
 end
