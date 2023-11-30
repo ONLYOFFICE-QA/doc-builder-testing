82cfde0b42c40439557c1d104d65ec1bb7bba176
Yar04ek <yaroslavmaslov0951@gmail.com>
2023-11-30 08:43:02 +0100

    feature: ApiRGBColor.ToJSON

--- spec/docx/smoke/api_rgb_color_spec.rb
+++ spec/docx/smoke/api_rgb_color_spec.rb
@@ -6,4 +6,13 @@ describe 'ApiRGBColor section tests' do
     docx = builder.build_and_parse('js/docx/smoke/api_rgb_color/get_class_type.js')
     expect(docx.elements[1].nonempty_runs.first.text).to eq('Class Type = rgbColor')
   end
+
+  it 'ApiRGBColor | ToJSON method' do
+    docx = builder.build_and_parse('js/docx/smoke/api_rgb_color/to_json.js')
+    first_paragraph_text = docx.elements[0].nonempty_runs.first.text
+    second_paragraph_text = docx.elements[1].nonempty_runs.first.text
+    first_paragraph_json = JSON.parse(first_paragraph_text)
+    second_paragraph_json = JSON.parse(second_paragraph_text)
+    expect(second_paragraph_json.to_s).to include(first_paragraph_json.to_s)
+  end
 end
