d8da9d6a6704232bf309f802ba03e49993526a58
Yarosalv Maslov <148754748+Yar04ek@users.noreply.github.com>
2023-12-02 17:46:28 +0100

    [7.6.0][CDE] ApiRGBColor.ToJSON check (#997)

---
+++ js/docx/smoke/api_rgb_color/to_json.js
@@ -0,0 +1,29 @@
+builder.CreateFile("docx");
+    let oDocument = Api.GetDocument();
+    let oParagraph = oDocument.GetElement(0);
+    let oRGBColor = Api.CreateRGBColor(255, 111, 61);
+    let jsonRGBColor = oRGBColor.ToJSON();
+    oParagraph.AddText(jsonRGBColor);
+    GlobalVariable["JSON_RGBColor"] = jsonRGBColor;
+builder.CloseFile();
+
+/////////////////////
+
+builder.CreateFile("docx");
+    let oDocument = Api.GetDocument();
+    let oParagraph1 = oDocument.GetElement(0);
+    let jsonRGBColor = GlobalVariable["JSON_RGBColor"];
+    let oRGBColor = Api.FromJSON(jsonRGBColor);
+        oParagraph1.AddText(jsonRGBColor);
+        oDocument.Push(oParagraph1);
+    let oParagraph2 = Api.CreateParagraph();
+    let oGs1 = Api.CreateGradientStop(oRGBColor, 0);
+    let oGs2 = Api.CreateGradientStop(255, 213, 191, 100000);
+    let oFill = Api.CreateLinearGradientFill([oGs1, oGs2], 5400000);
+    let oStroke = Api.CreateStroke(0, Api.CreateNoFill());
+    let oDrawing = Api.CreateShape("rect", 5930900, 395605, oFill, oStroke);
+    let jsonDrawing = oDrawing.ToJSON(false, true);
+        oParagraph2.AddText(jsonDrawing);
+        oDocument.Push(oParagraph2);
+builder.SaveFile("docx", "RGBCoolorToJson.docx");
+builder.CloseFile();
