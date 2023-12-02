7bfde89a98fe9aad115bce5281d900abea543754
Yarosalv Maslov <148754748+Yar04ek@users.noreply.github.com>
2023-12-02 17:46:28 +0100

    [7.6.0][CDE] ApiRGBColor.ToJSON check (#997)

--- spec/docx/smoke/api_rgb_color_spec.rb
+++ spec/docx/smoke/api_rgb_color_spec.rb
@@ -6,4 +6,13 @@ describe 'ApiRGBColor section tests' do
     docx = builder.build_and_parse('js/docx/smoke/api_rgb_color/get_class_type.js')
     expect(docx.elements[1].nonempty_runs.first.text).to eq('Class Type = rgbColor')
   end
+
+  it 'ApiRGBColor | ToJSON method' do
+    docx = builder.build_and_parse('js/docx/smoke/api_rgb_color/to_json.js')
+    json = JSON.parse(docx.elements[1].nonempty_runs.first.text)
+    rgba = json['graphic']['spPr']['fill']['fill']['gsLst'][0]['color']['color']['rgba']
+    expect(rgba['red']).to eq(255)
+    expect(rgba['green']).to eq(111)
+    expect(rgba['blue']).to eq(61)
+  end
 end
