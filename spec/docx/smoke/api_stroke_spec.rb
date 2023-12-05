75e5eee095b5a0517c1e4de26c68e11ea4484ed0
Yar04ek <yaroslavmaslov0951@gmail.com>
2023-12-05 14:00:04 +0100

    feature: ApiStroke.ToJSON

--- spec/docx/smoke/api_stroke_spec.rb
+++ spec/docx/smoke/api_stroke_spec.rb
@@ -7,12 +7,8 @@ describe 'ApiStroke section tests' do
     expect(docx.elements[1].nonempty_runs.first.text).to eq('Class Type = stroke')
   end
 
-  it 'ApiStroke | ToJSON method' do
+  it 'ApiRGBColor | ToJSON method' do
     docx = builder.build_and_parse('js/docx/smoke/api_stroke/to_json.js')
-    expect(docx.elements.first.nonempty_runs.first.alternate_content.office2010_content.graphic.data.text_body.elements.first.character_style_array.size).to eq(2)
-    expect(docx.elements.first.nonempty_runs.first
-               .alternate_content.office2010_content.graphic.type).to eq(:shape)
-    expect(docx.elements[1].nonempty_runs.first.text).to include('"type":"stroke"')
     json = JSON.parse(docx.elements[1].nonempty_runs.first.text)
     stroke = json['graphic']['spPr']['ln']['fill']['fill']['color']['color']['rgba']
     expect(stroke['red']).to eq(51)
