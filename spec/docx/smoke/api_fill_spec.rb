a23313a0b73f9a9d8d0c82eb86e8c298de49d91c
Yarosalv Maslov <148754748+Yar04ek@users.noreply.github.com>
2023-12-05 12:35:23 +0100

    feature: Fill.ToJSON (#1008)

--- spec/docx/smoke/api_fill_spec.rb
+++ spec/docx/smoke/api_fill_spec.rb
@@ -6,4 +6,17 @@ describe 'ApiFill section tests' do
     docx = builder.build_and_parse('js/docx/smoke/api_fill/get_class_type.js')
     expect(docx.elements.first.nonempty_runs[1].text).to eq('Class Type = fill')
   end
+
+  it 'ApiFill | ToJSON method' do
+    docx = builder.build_and_parse('js/docx/smoke/api_fill/to_json.js')
+    json = JSON.parse(docx.elements[1].nonempty_runs.first.text)
+    fill = json['graphic']['spPr']['fill']['fill']['gsLst'][0]['color']['color']['rgba']
+    fill1 = json['graphic']['spPr']['fill']['fill']['gsLst'][1]['color']['color']['rgba']
+    expect(fill['red']).to eq(255)
+    expect(fill['green']).to eq(111)
+    expect(fill['blue']).to eq(61)
+    expect(fill1['red']).to eq(255)
+    expect(fill1['green']).to eq(213)
+    expect(fill1['blue']).to eq(191)
+  end
 end
