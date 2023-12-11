1ac53e20ed8cc1354c1aa6d31cf3b71fe8853cea
Yar04ek <yaroslavmaslov0951@gmail.com>
2023-12-11 16:10:58 +0100

    ref: ApiUniColor.ToJSON

--- spec/docx/smoke/api_uni_color_spec.rb
+++ spec/docx/smoke/api_uni_color_spec.rb
@@ -10,11 +10,10 @@ describe 'ApiUniColor section tests' do
   it 'ApiUniColor | ToJSON method' do
     docx = builder.build_and_parse('js/docx/smoke/api_uni_color/to_json.js')
     json = JSON.parse(docx.elements[0].nonempty_runs.first.text)
-    color_type = json['graphic']['spPr']['fill']['fill']['color']['type']
-    rgba = json['graphic']['spPr']['fill']['fill']['color']['color']['rgba']
-    expect(color_type).to eq('uniColor')
-    expect(rgba['red']).to eq(255)
-    expect(rgba['green']).to eq(111)
-    expect(rgba['blue']).to eq(61)
+    color = json['graphic']['spPr']['fill']['fill']['color']
+    expect(color['type']).to eq('uniColor')
+    expect(color['color']['rgba']['red']).to eq(255)
+    expect(color['color']['rgba']['green']).to eq(111)
+    expect(color['color']['rgba']['blue']).to eq(61)
   end
 end
