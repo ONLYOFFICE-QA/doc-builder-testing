f0065548f8750ebe13469f22a47ddc94c4c23d78
Yarosalv Maslov <148754748+Yar04ek@users.noreply.github.com>
2023-12-12 12:14:20 +0100

    [7.6.0][CDE] ApiUniColor.ToJSON check  (#1015)
    * feature: ApiUniColor.ToJSON
    
    * ref: ApiUniColor.ToJSON
    
    * ref: ApiUniColor.ToJSON
    
    * ref: ApiUniColor.ToJSON
    
    * ref: ApiUniColor.ToJSON

--- spec/docx/smoke/api_uni_color_spec.rb
+++ spec/docx/smoke/api_uni_color_spec.rb
@@ -6,4 +6,14 @@ describe 'ApiUniColor section tests' do
     docx = builder.build_and_parse('js/docx/smoke/api_uni_color/get_class_type.js')
     expect(docx.elements[1].nonempty_runs.first.text).to eq('Class Type = presetColor')
   end
+
+  it 'ApiUniColor | ToJSON method' do
+    docx = builder.build_and_parse('js/docx/smoke/api_uni_color/to_json.js')
+    json = JSON.parse(docx.elements[0].nonempty_runs.first.text)
+    color = json['graphic']['spPr']['fill']['fill']['color']
+    expect(color['type']).to eq('uniColor')
+    expect(color['color']['rgba']['red']).to eq(255)
+    expect(color['color']['rgba']['green']).to eq(111)
+    expect(color['color']['rgba']['blue']).to eq(61)
+  end
 end
