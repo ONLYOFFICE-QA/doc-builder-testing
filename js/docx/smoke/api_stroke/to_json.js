2257de22e19869300086d9732d7976c9338e6059
Yar04ek <yaroslavmaslov0951@gmail.com>
2023-12-12 17:02:37 +0100

    ref: ApiStroke.ToJSON

--- js/docx/smoke/api_stroke/to_json.js
+++ js/docx/smoke/api_stroke/to_json.js
@@ -12,17 +12,17 @@ builder.CloseFile();
 
 builder.CreateFile("docx");
     let oDocument = Api.GetDocument();
-    let oParagraph1 = oDocument.GetElement(0);
+    let oParagraph = oDocument.GetElement(0);
     let jsonStroke = GlobalVariable["JSON_Stroke"];
-    let oStrokeFromJSON = Api.FromJSON(jsonStroke);
-        oParagraph1.AddText(jsonStroke);
-        oDocument.Push(oParagraph1);
-    let oParagraph2 = Api.CreateParagraph();
     let oRGBColor2 = Api.CreateRGBColor(255, 213, 191);
     let oFill = Api.CreateSolidFill(oRGBColor2);
+    let oStrokeFromJSON = Api.FromJSON(jsonStroke);
     let oDrawing = Api.CreateShape("roundRect", 5930900, 395605, oFill, oStrokeFromJSON);
+        oParagraph.AddDrawing(oDrawing);
+    let oParagraph2 = Api.CreateParagraph();
     let jsonDrawing = oDrawing.ToJSON(false, true);
         oParagraph2.AddText(jsonDrawing);
         oDocument.Push(oParagraph2);
 builder.SaveFile("docx", "StrokeToJson.docx");
 builder.CloseFile();
+
