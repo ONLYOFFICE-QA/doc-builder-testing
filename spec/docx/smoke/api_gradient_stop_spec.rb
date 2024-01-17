f2d93f35a523ce87dcd9417fd3f26d6660680f0d
Yarosalv Maslov <148754748+Yar04ek@users.noreply.github.com>
2024-01-17 12:50:40 +0100

    [7.6.0][CDE] ApiGradientStop.ToJSON check (#1014)
    * feature: ApiGradientStop.ToJSON
    
    * ApiGradientStop.ToJSON

--- spec/docx/smoke/api_gradient_stop_spec.rb
+++ spec/docx/smoke/api_gradient_stop_spec.rb
@@ -6,4 +6,17 @@ describe 'ApiGradientStop section tests' do
     docx = builder.build_and_parse('js/docx/smoke/api_gradient_stop/get_class_type.js')
     expect(docx.elements.first.nonempty_runs[1].text).to eq('Class Type = gradientStop')
   end
+
+  it 'ApiGradientStop | ToJSON method' do
+    docx = builder.build_and_parse('js/docx/smoke/api_gradient_stop/to_json.js')
+    json_drawing = JSON.parse(docx.elements[0].runs[3].text)
+    gs_list = json_drawing['graphic']['spPr']['fill']['fill']['gsLst']
+    start = gs_list[0]['color']['color']['rgba']
+    stop  = gs_list[1]['color']['color']['rgba']
+    actual = [OoxmlParser::Color.new(start['red'], start['green'], start['blue']),
+              OoxmlParser::Color.new(stop['red'], stop['green'], stop['blue'])]
+    expected = [OoxmlParser::Color.new(255, 111, 61),
+                OoxmlParser::Color.new(255, 213, 191)]
+    expect(actual).to eq(expected)
+  end
 end
