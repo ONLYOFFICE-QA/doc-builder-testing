05650d3e7229471678d7fafc4ca5b7e38019572c
MaxSorokin <askonev007@gmail.com>
2024-06-13 18:54:49 +0300

    [8.1][CDE] SchemeColor.ToJSON()

--- spec/docx/smoke/api_scheme_color_spec.rb
+++ spec/docx/smoke/api_scheme_color_spec.rb
@@ -6,4 +6,11 @@ describe 'ApiSchemeColor section tests' do
     docx = builder.build_and_parse('js/docx/smoke/api_scheme_color/get_class_type.js')
     expect(docx.elements[1].nonempty_runs.first.text).to eq('Class Type = schemeColor')
   end
+
+  it 'ApiSchemeColor | ToJSON method' do
+    docx = builder.build_and_parse('js/docx/smoke/api_scheme_color/to_json.js')
+    json = JSON.parse(docx.elements[1].nonempty_runs.first.text)
+    expect(docx.elements[0].nonempty_runs.first.text).to eq(json['type'])
+    expect(docx.elements[2].nonempty_runs[0].alternate_content.office2007_content.type).to eq(:shape)
+  end
 end
