2d2a4386a81c9dc9352b6dc2f3739f2947273be8
MaxSorokin <askonev007@gmail.com>
2024-06-18 18:10:07 +0300

    [8.1] set color scheme

--- js/xlsx/smoke/api/set_theme_colors.js
+++ js/xlsx/smoke/api/set_theme_colors.js
@@ -1,9 +1,9 @@
 builder.CreateFile("xlsx");
-var oWorksheet = Api.GetActiveSheet();
-var themes = Api.GetThemesColors();
-for (var i = 0; i < themes.length; ++i) {
-oWorksheet.GetRange('A' + (i + 1)).SetValue(themes[i]);
-}
-Api.SetThemeColors(themes[3]);
+    var oWorksheet = Api.GetActiveSheet();
+    var themes = Api.GetThemesColors();
+        for (var i = 0; i < themes.length; ++i) {
+        oWorksheet.GetRange('A' + (i + 1)).SetValue(themes[i]);
+        }
+        Api.SetThemeColors(themes[3]);
 builder.SaveFile("xlsx", "SetThemeColors.xlsx");
 builder.CloseFile();
