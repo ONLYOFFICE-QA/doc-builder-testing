051a9ff7d659024adb6a7a405521c1f6c8c4cad2
lemonowl <tcm.panika@gmail.com>
2024-07-04 09:15:20 +0300

    pr comments fix

--- spec/docx/smoke/api_table_spec.rb
+++ spec/docx/smoke/api_table_spec.rb
@@ -190,8 +190,7 @@ describe 'ApiTable section tests' do
 
   describe 'ApiTable | AddCaption method' do
     before(:all) do
-      @docx = builder.build_and_parse('js/docx/smoke/api_table/add_caption.js')
-      @docx.freeze
+      @docx = builder.build_and_parse('js/docx/smoke/api_table/add_caption.js').freeze
     end
 
     def check_style(element)
