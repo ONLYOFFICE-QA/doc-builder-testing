e0fb04d3e7cf4eafbe31f655aa4261656cd3caf4
lemonowl <tcm.panika@gmail.com>
2024-08-13 14:57:53 +0300

    fix

--- js/docx/negative/api_range/get_range.js
+++ js/docx/negative/api_range/get_range.js
@@ -3,7 +3,7 @@ var oArgument = Argument;
 var oDocument = Api.GetDocument();
 var oParagraph = oDocument.GetElement(0);
 oParagraph.AddText("Document Builder check GetRange method");
-var oRange1 = oDocument.GetRange(oArgument['start_pos'], oArgument['end_pos']);
+var oRange1 = oDocument.GetRange(oArgument["start_pos"], oArgument["end_pos"]);
 oRange1.SetBold(true);
 builder.SaveFile("docx", "GetRange.docx");
 builder.CloseFile();
