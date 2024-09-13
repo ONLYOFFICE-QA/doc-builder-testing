748242c894ed14c8cc6542b0ecbecebe7bd051d8
lemonowl <tcm.panika@gmail.com>
2024-09-13 17:35:20 +0300

    fix

--- python/wrapper/smoke/doc_builder/run_text.py
+++ python/wrapper/smoke/doc_builder/run_text.py
@@ -1,6 +1,7 @@
 from python.docbuilder import builder
 builder.CreateFile("docx")
-builder.RunText("""var oDocument = Api.GetDocument();
+builder.RunText("""
+var oDocument = Api.GetDocument();
 var oParagraph = Api.CreateParagraph();
 oParagraph.AddText("This is a paragraph for RunText");
 oDocument.Push(oParagraph);
