310501e4a7489c3128b36f15504f0da5ab02f66f
MaxSorokin <askonev007@gmail.com>
2024-10-17 16:39:57 +0300

    fix: page size preset 63370

--- js/docx/sections/page_size_for_section.js
+++ js/docx/sections/page_size_for_section.js
@@ -1,11 +1,14 @@
 builder.CreateFile("docx");
-var oDocument = Api.GetDocument();
-oDocument.CreateNewHistoryPoint();
-var oParagraph1, oFinalSection;
-oParagraph = Api.CreateParagraph();
-oDocument.Push(oParagraph);
-oSection = oDocument.CreateSection(oParagraph);
-oFinalSection = oDocument.GetFinalSection();
-oFinalSection.SetPageSize(12240, 15840);
+    var oDocument = Api.GetDocument();
+        oDocument.CreateNewHistoryPoint();
+    var oParagraph, oFinalSection, oSection;
+        oFinalSection = oDocument.GetFinalSection();
+        oFinalSection.SetPageSize(11904.8, 16838); // 'A4'
+
+        oParagraph = Api.CreateParagraph();
+        oParagraph.AddText('section');
+        oDocument.Push(oParagraph);
+        oSection = oDocument.CreateSection(oParagraph);
+        oSection.SetPageSize(12240, 15840); // 'US Letter'
 builder.SaveFile("docx", "page_size_for_section.docx");
 builder.CloseFile();
