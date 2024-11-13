420b0808614657bc7ea206785a509ad5d2b05841
MaxSorokin <askonev007@gmail.com>
2024-11-13 19:51:25 +0300

    regression: 66605 RecalculateAllFormulas & ApiRange.Paste

--- spec/xlsx/regression/api_range_spec.rb
+++ spec/xlsx/regression/api_range_spec.rb
@@ -7,4 +7,12 @@ describe 'Range Api regression' do
     expect(xlsx.worksheets.first.rows[7].cells[6].raw_text).to eq('1')
     expect(xlsx.worksheets.first.rows[7].cells[6].style.fill_color.pattern_fill.background_color).to eq(OoxmlParser::Color.new(255, 213, 191))
   end
+
+  it 'RecalculateAllFormulas | 66605' do
+    xlsx = builder.build_and_parse('js/xlsx/specific/api_range/66605.js')
+    expect(xlsx.worksheets[1].rows[8].cells[4]).to eq('0')
+    # TODO: https://bugzilla.onlyoffice.com/show_bug.cgi?id=71637
+    # expect(JSON.parse(xlsx.worksheets[1].rows[10].cells[0])).to have_no_empty_string_values
+    # expect(xlsx.worksheets[1].rows[11].cells[0]).to eq('true')
+  end
 end
