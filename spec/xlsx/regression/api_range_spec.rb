edfc04cd2ede47a8c99786ada681ada3752e59c1
MaxSorokin <askonev007@gmail.com>
2024-11-13 20:13:40 +0300

    fix: misspell

--- spec/xlsx/regression/api_range_spec.rb
+++ spec/xlsx/regression/api_range_spec.rb
@@ -10,7 +10,7 @@ describe 'Range Api regression' do
 
   it 'RecalculateAllFormulas | 66605' do
     xlsx = builder.build_and_parse('js/xlsx/regression/api_range/66605.js')
-    expect(xlsx.worksheets[1].rows[8].cells[4]).to eq('0')
+    expect(xlsx.worksheets[1].rows[8].cells[4].raw_text).to eq('0')
     # TODO: https://bugzilla.onlyoffice.com/show_bug.cgi?id=71637
     # expect(JSON.parse(xlsx.worksheets[1].rows[10].cells[0])).to have_no_empty_string_values
     # expect(xlsx.worksheets[1].rows[11].cells[0]).to eq('true')
