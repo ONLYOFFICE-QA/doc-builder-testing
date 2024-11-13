f0e796e522b81c1ff13f5337b5b3587d11e843f3
MaxSorokin <askonev007@gmail.com>
2024-11-13 19:56:17 +0300

    ref: for xlsx specific to regression

--- spec/xlsx/regression/api_range_spec.rb
+++ spec/xlsx/regression/api_range_spec.rb
@@ -3,13 +3,13 @@
 require 'spec_helper'
 describe 'Range Api regression' do
   it 'Find | 67358' do
-    xlsx = builder.build_and_parse('js/xlsx/specific/api_range/67358.js')
+    xlsx = builder.build_and_parse('js/xlsx/regression/api_range/67358.js')
     expect(xlsx.worksheets.first.rows[7].cells[6].raw_text).to eq('1')
     expect(xlsx.worksheets.first.rows[7].cells[6].style.fill_color.pattern_fill.background_color).to eq(OoxmlParser::Color.new(255, 213, 191))
   end
 
   it 'RecalculateAllFormulas | 66605' do
-    xlsx = builder.build_and_parse('js/xlsx/specific/api_range/66605.js')
+    xlsx = builder.build_and_parse('js/xlsx/regression/api_range/66605.js')
     expect(xlsx.worksheets[1].rows[8].cells[4]).to eq('0')
     # TODO: https://bugzilla.onlyoffice.com/show_bug.cgi?id=71637
     # expect(JSON.parse(xlsx.worksheets[1].rows[10].cells[0])).to have_no_empty_string_values
--- spec/xlsx/regression/api_worksheet_spec.rb
+++ spec/xlsx/regression/api_worksheet_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 describe 'Worksheet Api regression' do
   it 'GetCells | 56159' do
-    xlsx = builder.build_and_parse('js/xlsx/specific/api_worksheet/56159.js')
+    xlsx = builder.build_and_parse('js/xlsx/regression/api_worksheet/56159.js')
     expect(xlsx.worksheets.first.rows.first.cells.first.raw_text).to eq('first cell')
   end
 end
