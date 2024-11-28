b19bb280ed18747ed9b1019ec7e836fb11e2ad22
Olga Petrova <58073620+lemonowl@users.noreply.github.com>
2024-11-28 11:53:08 +0300

    skip for web builder (#1278)

--- spec/xlsx/regression/api_range_spec.rb
+++ spec/xlsx/regression/api_range_spec.rb
@@ -9,6 +9,7 @@ describe 'Range Api regression' do
   end
 
   it 'RecalculateAllFormulas | 66605' do
+    skip('Test require local file to open and not available in web builder') if web_builder?
     xlsx = builder.build_and_parse('js/xlsx/regression/api_range/66605.js')
     expect(xlsx.worksheets[1].rows[8].cells[4].raw_text).to eq('0')
     # TODO: https://bugzilla.onlyoffice.com/show_bug.cgi?id=71637
