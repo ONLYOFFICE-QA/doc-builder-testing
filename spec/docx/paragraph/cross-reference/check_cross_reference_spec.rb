dc0dd7d5759b1becd7a980c135853bfb63a83605
Olga Petrova <olga.petrova@onlyoffice.com>
2025-02-04 16:19:53 +0300

    test fix

--- spec/docx/paragraph/cross-reference/check_cross_reference_spec.rb
+++ spec/docx/paragraph/cross-reference/check_cross_reference_spec.rb
@@ -35,12 +35,18 @@ describe 'All tests numbering object' do
   it 'Cross-reference to bookmarks' do
     docx = builder.build_and_parse('js/docx/paragraph/cross_reference/add_bookmark_cross_ref.js')
     expect(docx.elements[1].nonempty_runs[1].text).to eq('Paragraph')
-    expect(docx.elements[1].runs[1].instruction_text.value).to eq(' REF Bookmark  \\h')
+    if builder.semver >= Semantic::Version.new('8.3.0')
+      expect(docx.elements[1].runs[1].instruction_text.value).to eq(' REF Bookmark  \\h')
+      expect(docx.elements[2].runs[1].instruction_text.value).to eq(' PAGEREF Bookmark \\h')
+      expect(docx.elements[3].runs[1].instruction_text.value).to eq(' REF Bookmark \\r  \\h')
+    else
+      expect(docx.elements[1].runs[2].instruction_text.value).to eq(' REF Bookmark  \\h')
+      expect(docx.elements[2].runs[2].instruction_text.value).to eq(' PAGEREF Bookmark \\h')
+      expect(docx.elements[3].runs[2].instruction_text.value).to eq(' REF Bookmark \\r  \\h')
+    end
     # TODO: bug 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=58898'
     # expect(docx.elements[2].nonempty_runs[1].text).to eq('1')
-    expect(docx.elements[2].runs[1].instruction_text.value).to eq(' PAGEREF Bookmark \\h')
     expect(docx.elements[3].nonempty_runs[1].text).to eq('0')
-    expect(docx.elements[3].runs[1].instruction_text.value).to eq(' REF Bookmark \\r  \\h')
     expect(docx.elements[4].nonempty_runs.first.text).to eq('above')
     expect(docx.elements[4].runs[1].instruction_text.value).to eq(' REF Bookmark \\p  \\h')
   end
