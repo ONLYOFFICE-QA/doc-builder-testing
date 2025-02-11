3dcc5c093a98b5c9343e41e63d1b1650650b0d35
MaxSorokin <askonev007@gmail.com>
2025-02-11 19:20:18 +0300

    ref: strict mode

--- js/pptx/smoke/api_presentation/get_slide_by_index.js
+++ js/pptx/smoke/api_presentation/get_slide_by_index.js
@@ -1,9 +1,9 @@
 builder.CreateFile("pptx");
-var oPresentation = Api.GetPresentation();
-var oSlide = oPresentation.GetSlideByIndex(0);
-oGs1 = Api.CreateGradientStop(Api.CreateRGBColor(255, 224, 204), 0);
-oGs2 = Api.CreateGradientStop(Api.CreateRGBColor(255, 164, 101), 100000);
-oFill = Api.CreateRadialGradientFill([oGs1, oGs2]);
-oSlide.SetBackground(oFill);
+    var oPresentation = Api.GetPresentation();
+    var oSlide = oPresentation.GetSlideByIndex(0);
+    var oGs1 = Api.CreateGradientStop(Api.CreateRGBColor(255, 224, 204), 0);
+    var oGs2 = Api.CreateGradientStop(Api.CreateRGBColor(255, 164, 101), 100000);
+    var oFill = Api.CreateRadialGradientFill([oGs1, oGs2]);
+        oSlide.SetBackground(oFill);
 builder.SaveFile("pptx", "GetSlideByIndex.pptx");
 builder.CloseFile();
