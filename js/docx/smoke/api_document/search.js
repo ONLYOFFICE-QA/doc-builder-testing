67b0fd9fa7e64f66a3d025dc82a3f8bc0c741919
Olga Petrova <olga.petrova@onlyoffice.com>
2025-02-17 11:29:42 +0300

    ApiDocumet|ApiParagraph.Search

--- js/docx/smoke/api_document/search.js
+++ js/docx/smoke/api_document/search.js
@@ -1,15 +1,22 @@
 builder.CreateFile("docx");
 var oDocument = Api.GetDocument();
 var oParagraph = oDocument.GetElement(0);
-oParagraph.AddText("This is text for paragraph Search method");
-oParagraph.AddLineBreak();
-var oRun1 = Api.CreateRun();
-oRun1.AddText("This is text");
-oParagraph.Push(oRun1);
-oRun1.AddHyperlink("https://api.onlyoffice.com/", "link");
-oParagraph.AddText(" for paragraph Search method");
+oParagraph.AddText("This is text for document Search method");
 
-var oSearch = oParagraph.Search("search");
+oParagraph = Api.CreateParagraph();
+var oRun1 = Api.CreateRun();
+oRun1.AddText("This is");
+oParagraph.Push(oRun1);
+oRun1.AddHyperlink("https://api.onlyoffice.com/", "first link");
+oParagraph.AddText(" ");
+var oRun2 = Api.CreateRun();
+oRun2.AddText("text for document");
+oParagraph.Push(oRun2);
+oRun2.AddHyperlink("https://api.onlyoffice.com/", "second link");
+oParagraph.AddText(" Search method");
+oDocument.Push(oParagraph);
+
+var oSearch = oDocument.Search("search");
 var aSearchResult = [];
 for (var i = 0; i < oSearch.length; i++) {
     aSearchResult.push(oSearch[i].GetText());
