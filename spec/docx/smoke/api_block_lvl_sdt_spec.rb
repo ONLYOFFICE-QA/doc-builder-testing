48a401a63e35c9cec8bffcd93e5def69c777efba
MaxSorokin <askonev007@gmail.com>
2025-02-18 17:37:50 +0300

    fix: remove check to numbering obj

--- spec/docx/smoke/api_block_lvl_sdt_spec.rb
+++ spec/docx/smoke/api_block_lvl_sdt_spec.rb
@@ -5,10 +5,10 @@ require 'spec_helper'
 describe 'ApiBlockLvlSdt section tests' do
   it 'ApiBlockLvlSdt | ToJSON method' do
     docx = builder.build_and_parse('js/docx/smoke/api_block_lvl_sdt/to_json.js')
-    expect(docx.elements[0].sdt_content.elements[0].character_style_array[1].text).to eq('blockLvlSdt')
-    expect(docx.elements[0].sdt_content.elements[8].numbering.i_level.value).to eq(7)
+    set_text = docx.elements[0].sdt_content.elements[0].character_style_array[1].text
+    expect(set_text).to eq('blockLvlSdt')
     json = JSON.parse(docx.elements[1].nonempty_runs.first.text)
-    expect(json['type']).to eq('blockLvlSdt')
+    expect(json['type']).to eq(set_text)
     expect(json['numbering']['type']).to eq('numbering')
   end
 end
