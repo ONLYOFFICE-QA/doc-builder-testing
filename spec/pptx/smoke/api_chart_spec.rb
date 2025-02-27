929308c6628401d897a33a9647d11683a21dcf40
Olga Petrova <olga.petrova@onlyoffice.com>
2025-02-27 12:55:55 +0300

    codestyle

--- spec/pptx/smoke/api_chart_spec.rb
+++ spec/pptx/smoke/api_chart_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 describe 'ApiChart section tests' do
-  before { skip('https://bugzilla.onlyoffice.com/show_bug.cgi?id=73379') if builder.semver > Semantic::Version.new('8.3.0')}
+  before { skip('https://bugzilla.onlyoffice.com/show_bug.cgi?id=73379') if builder.semver > Semantic::Version.new('8.3.0') }
 
   it 'ApiChart | GetClassType method' do
     pptx = builder.build_and_parse('js/pptx/smoke/api_chart/get_class_type.js')
