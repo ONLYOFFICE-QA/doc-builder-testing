7bedd7c942f5df1f30614336fccf3f60cbeeb938
MaxSorokin <askonev007@gmail.com>
2025-05-21 19:24:26 +0300

    fix: build

--- dockerfiles/debian-next-release/portable.sh
+++ dockerfiles/debian-next-release/portable.sh
@@ -2,8 +2,8 @@
 
 set -e  # Exit immediately if a command exits with non-zero status
 
-URL='https://s3.eu-west-1.amazonaws.com/repo-doc-onlyoffice-com/builder/mac/generic/onlyoffice-documentbuilder-9.0.0-116-x86_64.tar.xz'
-SHA256='3ad5d32ee3c08ceadedeb4c1759050e3d3bf2f75b99eae6553c0648e8331b08f'
+URL='https://s3.eu-west-1.amazonaws.com/repo-doc-onlyoffice-com/builder/linux/generic/onlyoffice-documentbuilder-9.0.0-116-x86_64.tar.xz'
+SHA256='8d42db4d2e06b78b9dd23778285657c52276804f52c1290d7f546cbf6d04b3ce'
 
 DOWNLOADED_FILE=$(basename "$URL")
 
