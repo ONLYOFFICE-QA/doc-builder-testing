50a7235675ed173b821355399954a262c5e2a0e7
Vitaly Instrumentov <regent112rec@gmail.com>
2025-10-14 13:15:10 +0300

    change version

--- dockerfiles/debian-next-release/portable.sh
+++ dockerfiles/debian-next-release/portable.sh
@@ -2,8 +2,8 @@
 
 set -e  # Exit immediately if a command exits with non-zero status
 
-URL='https://s3.eu-west-1.amazonaws.com/repo-doc-onlyoffice-com/builder/linux/generic/onlyoffice-documentbuilder-9.1.0-165-x86_64.tar.xz'
-SHA256='c25fec2191aa9f991de92e755d28598258db6be18ceb9e5e4f3fb143010efac8'
+URL='https://s3.eu-west-1.amazonaws.com/repo-doc-onlyoffice-com/builder/linux/generic/onlyoffice-documentbuilder-9.1.0-167-x86_64.tar.xz'
+SHA256='f04170a9b67913e3d7f3d5f5badd7e132e9bb672d2b93cc4cebf59438607c3ab'
 
 DOWNLOADED_FILE=$(basename "$URL")
 
