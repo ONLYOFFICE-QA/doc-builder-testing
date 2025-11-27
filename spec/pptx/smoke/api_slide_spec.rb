862717ffb47f8a18b8bde3802927504be6972bca
Vitaly Instrumentov <vitaliy.instrumentov@onlyoffice.com>
2025-11-27 18:35:09 +0300

    fix skip for old version

--- spec/pptx/smoke/api_slide_spec.rb
+++ spec/pptx/smoke/api_slide_spec.rb
@@ -54,6 +54,7 @@ describe 'ApiSlide section tests' do
 
   it 'ApiSlide | Select' do
     # skip('https://bugzilla.onlyoffice.com/show_bug.cgi?id=73191')
+    skip('not implemented') if builder.semver < Semantic::Version.new('8.3.0')
     pptx = builder.build_and_parse('js/pptx/smoke/api_slide/select.js')
     slide_data = pptx.slides[0].common_slide_data.shape_tree
     expect(slide_data.elements[0].text_body.paragraphs[0].runs[0].text).to eq('is empty: false')
