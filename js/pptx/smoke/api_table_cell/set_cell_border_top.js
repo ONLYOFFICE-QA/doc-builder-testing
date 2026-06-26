590be46268bdf0ae4d478ae314b3f7c26a7d0657
Vitaly Instrumentov <vitaliy.instrumentov@onlyoffice.com>
2026-06-26 12:17:17 +0300

    modify builder files for new version SetCellBorder methods > add first param BorderType

--- js/pptx/smoke/api_table_cell/set_cell_border_top.js
+++ js/pptx/smoke/api_table_cell/set_cell_border_top.js
@@ -1,8 +1,10 @@
 builder.CreateFile("pptx");
+
 var Presentation = Api.GetPresentation();
 var Slide = Presentation.GetSlideByIndex(0);
 var Table = Api.CreateTable(1,1);
 Slide.AddObject(Table);
-Table.GetRow(0).GetCell(0).SetCellBorderTop(5, Api.CreateSolidFill (Api.CreateRGBColor (255, 0, 0)));
+Table.GetRow(0).GetCell(0).SetCellBorderTop("single", 5, Api.CreateSolidFill (Api.CreateRGBColor (255, 0, 0)));
+
 builder.SaveFile("pptx", "SetCellBorderTop.pptx");
 builder.CloseFile();
