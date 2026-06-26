35016ac291fdc3aefc1c806d43b7737b7e86495c
Vitaly Instrumentov <32766557+regent112@users.noreply.github.com>
2026-06-26 13:22:05 +0300

    modify builder files for new version SetCellBorder methods > add first param BorderType (#1553)
    * modify builder files for new version SetCellBorder methods > add first param BorderType
    
    * skip tests if old version builder
    
    ---------
    
    Co-authored-by: Vitaly Instrumentov <vitaliy.instrumentov@onlyoffice.com>

--- js/pptx/smoke/api_table_cell/set_cell_border_bottom.js
+++ js/pptx/smoke/api_table_cell/set_cell_border_bottom.js
@@ -1,8 +1,10 @@
 builder.CreateFile("pptx");
+
 var Presentation = Api.GetPresentation();
 var Slide = Presentation.GetSlideByIndex(0);
 var Table = Api.CreateTable(1,1);
 Slide.AddObject(Table);
-Table.GetRow(0).GetCell(0).SetCellBorderBottom(5, Api.CreateSolidFill (Api.CreateRGBColor (255, 0, 0)));
+Table.GetRow(0).GetCell(0).SetCellBorderBottom("single", 5, Api.CreateSolidFill (Api.CreateRGBColor (255, 0, 0)));
+
 builder.SaveFile("pptx", "SetCellBorderBottom.pptx");
 builder.CloseFile();
--- js/pptx/smoke/api_table_cell/set_cell_border_right.js
+++ js/pptx/smoke/api_table_cell/set_cell_border_right.js
@@ -1,8 +1,10 @@
 builder.CreateFile("pptx");
+
 var Presentation = Api.GetPresentation();
 var Slide = Presentation.GetSlideByIndex(0);
 var Table = Api.CreateTable(1,1);
 Slide.AddObject(Table);
-Table.GetRow(0).GetCell(0).SetCellBorderRight(5, Api.CreateSolidFill (Api.CreateRGBColor (255, 0, 0)));
+Table.GetRow(0).GetCell(0).SetCellBorderRight("single", 5, Api.CreateSolidFill (Api.CreateRGBColor (255, 0, 0)));
+
 builder.SaveFile("pptx", "SetCellBorderRight.pptx");
 builder.CloseFile();
