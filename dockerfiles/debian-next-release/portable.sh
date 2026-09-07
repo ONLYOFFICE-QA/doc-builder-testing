37618977a8e7201a8c8ab7fa346caabd64a5325d
Vitaly Instrumentov <32766557+regent112@users.noreply.github.com>
2026-09-07 09:36:03 +0300

    new version builder (#1569)
    Co-authored-by: Vitaly Instrumentov <vitaliy.instrumentov@onlyoffice.com>

--- dockerfiles/debian-next-release/portable.sh
+++ dockerfiles/debian-next-release/portable.sh
@@ -2,8 +2,8 @@
 
 set -e  # Exit immediately if a command exits with non-zero status
 
-URL='https://s3.eu-west-1.amazonaws.com/repo-doc-onlyoffice-com/builder/linux/generic/onlyoffice-documentbuilder-10.0.0-66-x86_64.tar.xz'
-SHA256='0994d31b79c3ea4e8166d9b79c69ae57f821ec155549fa5e47206d12702a1f0f'
+URL='https://s3.eu-west-1.amazonaws.com/repo-doc-onlyoffice-com/builder/linux/generic/onlyoffice-documentbuilder-10.0.0-73-x86_64.tar.xz'
+SHA256='d93a23bc375ff81d4259d7a835eda307cc4551c40321251c0de093befe46e57d'
 
 DOWNLOADED_FILE=$(basename "$URL")
 
