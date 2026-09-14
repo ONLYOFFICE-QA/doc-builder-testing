1e50ece3779ec749c3d1980b17dc65a4ceb338a6
Vitaly Instrumentov <32766557+regent112@users.noreply.github.com>
2026-09-14 17:18:16 +0300

    New version builder + skip test for bug83824 (#1574)
    * new version builder
    
    * skip for bug83824
    
    ---------
    
    Co-authored-by: Vitaly Instrumentov <vitaliy.instrumentov@onlyoffice.com>

--- dockerfiles/debian-next-release/portable.sh
+++ dockerfiles/debian-next-release/portable.sh
@@ -2,8 +2,8 @@
 
 set -e  # Exit immediately if a command exits with non-zero status
 
-URL='https://s3.eu-west-1.amazonaws.com/repo-doc-onlyoffice-com/builder/linux/generic/onlyoffice-documentbuilder-10.0.0-78-x86_64.tar.xz'
-SHA256='ee776f9449a62511882f2d1028c4749f57563dc198fff28d0f930a76492c7359'
+URL='https://s3.eu-west-1.amazonaws.com/repo-doc-onlyoffice-com/builder/linux/generic/onlyoffice-documentbuilder-10.0.0-84-x86_64.tar.xz'
+SHA256='d61b758c1e893ce5700ab23b623d05170315ef2cb4cf1313e6a02fad39c4a691'
 
 DOWNLOADED_FILE=$(basename "$URL")
 
